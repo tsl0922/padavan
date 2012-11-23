tsl0922/padavan

commit 7d23b547487bf887c7ad8df290c6db29623dcf6f

nvram: code refactoring, remove mmap, used ioctl for prevent kernel memory corruption

trunk/linux-2.6.21.x/drivers/nvram/nvram_linux.c
@@ -13,12 +13,12 @@
  */
 #define ASUS_NVRAM
 
-#include <linux/config.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/string.h>
 #include <linux/spinlock.h>
+#include <linux/sched.h>
 
 #include <linux/mm.h>
 #include <linux/version.h>
@@ -31,21 +31,19 @@
 
 #include "nvram.c"
 
-
-#define MTD_NVRAM_NAME	"Config"
-
-#define mem_map_reserve(p)		set_bit(PG_reserved, &((p)->flags))
-#define mem_map_unreserve(p)		clear_bit(PG_reserved, &((p)->flags))
+#define NVRAM_DRIVER_VERSION	"0.04"
+#define MTD_NVRAM_NAME		"Config"
+#define NVRAM_VALUES_SPACE	(NVRAM_SPACE*2)
 
 extern int ra_mtd_read_nm(char *name, loff_t from, size_t len, u_char *buf);
 extern int ra_mtd_write_nm(char *name, loff_t to, size_t len, const u_char *buf);
 
 /* Globals */
-static spinlock_t nvram_lock = SPIN_LOCK_UNLOCKED;
-static struct semaphore nvram_sem;
+static DEFINE_SPINLOCK(nvram_lock);
+static DEFINE_MUTEX(nvram_sem);
 static int nvram_major = -1;
 static struct proc_dir_entry *g_pdentry = NULL;
-static char *nvram_buf = NULL;
+static char *nvram_values = NULL;
 static unsigned long nvram_offset = 0;
 
 // from src/shared/bcmutils.c
@@ -122,8 +120,6 @@ hndcrc8(
 	return crc;
 }
 
-#define NVRAM_DRIVER_VERSION	"0.03"
-
 static int 
 nvram_proc_version_read(char *buf, char **start, off_t offset, int count, int *eof, void *data)
 {
@@ -139,7 +135,7 @@ nvram_proc_version_read(char *buf, char **start, off_t offset, int count, int *e
 		len += snprintf (buf+len, count-len, "  name       : %s\n", nvram_mtd->name);
 		len += snprintf (buf+len, count-len, "  index      : %d\n", nvram_mtd->index);
 		len += snprintf (buf+len, count-len, "  flags      : 0x%x\n", nvram_mtd->flags);
-		len += snprintf (buf+len, count-len, "  size       : 0x%x\n", nvram_mtd->size);
+		len += snprintf (buf+len, count-len, "  size       : 0x%llx\n", nvram_mtd->size);
 		len += snprintf (buf+len, count-len, "  erasesize  : 0x%x\n", nvram_mtd->erasesize);
 		
 		put_mtd_device(nvram_mtd);
@@ -163,27 +159,26 @@ _nvram_read_mtd(unsigned char *buf)
 struct nvram_tuple *
 _nvram_realloc(struct nvram_tuple *t, const char *name, const char *value)
 {
-	if ((nvram_offset + strlen(value) + 1) > NVRAM_SPACE) {
+	unsigned long val_len = (strlen(value) + 1);
+
+	if ((nvram_offset + val_len) >= NVRAM_VALUES_SPACE)
 		return NULL;
-	}
 
 	if (!t) {
 		if (!(t = kmalloc(sizeof(struct nvram_tuple) + strlen(name) + 1, GFP_ATOMIC))) {
 			return NULL;
 		}
-
 		/* Copy name */
 		t->name = (char *) &t[1];
 		strcpy(t->name, name);
-
 		t->value = NULL;
 	}
 
 	/* Copy value */
 	if (!t->value || strcmp(t->value, value)) {
-		t->value = &nvram_buf[nvram_offset];
+		t->value = &nvram_values[nvram_offset];
 		strcpy(t->value, value);
-		nvram_offset += strlen(value) + 1;
+		nvram_offset += val_len;
 	}
 
 	return t;
@@ -312,7 +307,7 @@ nvram_commit(void)
 	if (ret)
 		goto done;
 	
-	down(&nvram_sem);
+	mutex_lock(&nvram_sem);
 	
 	/* Write partition up to end of data area */
 	ret = ra_mtd_write_nm(MTD_NVRAM_NAME, NVRAM_MTD_OFFSET, NVRAM_SPACE, buf);
@@ -320,7 +315,7 @@ nvram_commit(void)
 		printk("nvram_commit: write error\n");
 	}
 	
-	up(&nvram_sem);
+	mutex_unlock(&nvram_sem);
 	
  done:
 	
@@ -353,114 +348,138 @@ EXPORT_SYMBOL(nvram_commit);
 EXPORT_SYMBOL(nvram_clear);
 
 /* User mode interface below */
-
-static ssize_t
-dev_nvram_read(struct file *file, char *buf, size_t count, loff_t *ppos)
+int
+user_nvram_set(anvram_ioctl_t __user *nvr)
 {
-	char tmp[100], *name = tmp, *value;
-	ssize_t ret;
-	unsigned long off;
-	
-	if (count > sizeof(tmp)) {
-		if (!(name = kmalloc(count, GFP_KERNEL)))
+	int ret;
+	char param[NVRAM_MAX_PARAM_LEN];
+	char tmp[64], *value;
+
+	if (!nvr)
+		return -EINVAL;
+
+	if (nvr->size != sizeof(anvram_ioctl_t))
+		return -EINVAL;
+
+	if (nvr->len_param > (NVRAM_MAX_PARAM_LEN-1) || nvr->len_param < 0)
+		return -EOVERFLOW;
+
+	if (copy_from_user(param, nvr->param, nvr->len_param))
+		return -EFAULT;
+
+	param[nvr->len_param] = '\0';
+	if (!param[0])
+		return -EINVAL;
+
+	if (nvr->len_value > (NVRAM_MAX_VALUE_LEN-1) || nvr->len_value < 0)
+		return -EOVERFLOW;
+
+	value = tmp;
+	if ((nvr->len_value+1) > sizeof(tmp)) {
+		if (!(value = kmalloc(nvr->len_value+1, GFP_KERNEL)))
 			return -ENOMEM;
 	}
 
-	if (copy_from_user(name, buf, count)) {
-		ret = -EFAULT;
-		goto done;
-	}
-
-	if (*name == '\0') {
-		/* Get all variables */
-		ret = nvram_getall(name, count);
-		if (ret == 0) {
-			if (copy_to_user(buf, name, count)) {
-				ret = -EFAULT;
-				goto done;
-			}
-			ret = count;
-		}
-	} else {
-		if (!(value = nvram_get(name))) {
-			ret = 0;
-			goto done;
-		}
-
-		/* Provide the offset into mmap() space */
-		off = (unsigned long) value - (unsigned long) nvram_buf;
-
-		if (put_user(off, (unsigned long *) buf)) {
+	if (nvr->len_value > 0) {
+		if (copy_from_user(value, nvr->value, nvr->len_value)) {
 			ret = -EFAULT;
 			goto done;
 		}
-
-		ret = sizeof(unsigned long);
 	}
 
+	value[nvr->len_value] = '\0';
+
+	if (value[0])
+		ret = nvram_set(param, value);
+	else
+		ret = nvram_unset(param);
+
 done:
-	if (name != tmp)
-		kfree(name);
+	if (value != tmp)
+		kfree(value);
 
 	return ret;
 }
 
-static ssize_t
-dev_nvram_write(struct file *file, const char *buf, size_t count, loff_t *ppos)
+int
+user_nvram_get(anvram_ioctl_t __user *nvr)
 {
-	char tmp[100], *name = tmp, *value;
-	ssize_t ret;
+	int len, ret;
+	char param[NVRAM_MAX_PARAM_LEN];
+	char *value;
 
-	if (count > sizeof(tmp)) {
-		if (!(name = kmalloc(count, GFP_KERNEL)))
+	if (!nvr)
+		return -EINVAL;
+
+	if (nvr->size != sizeof(anvram_ioctl_t))
+		return -EINVAL;
+
+	if (nvr->len_value < 1)
+		return -EINVAL;
+
+	if (nvr->len_param > (NVRAM_MAX_PARAM_LEN-1) || nvr->len_param < 0)
+		return -EINVAL;
+
+	if (nvr->len_param > 0) {
+		if (copy_from_user(param, nvr->param, nvr->len_param))
+			return -EFAULT;
+	}
+
+	param[nvr->len_param] = '\0';
+
+	ret = 0;
+
+	if (param[0] == '\0') {
+		if (nvr->len_value < NVRAM_SPACE) {
+			nvr->len_value = NVRAM_SPACE;
+			return -EOVERFLOW;
+		}
+		
+		if (!(value = (char*)kmalloc(NVRAM_SPACE, GFP_KERNEL)))
 			return -ENOMEM;
+		
+		ret = nvram_getall(value, NVRAM_SPACE);
+		if (ret == 0) {
+			nvr->len_value = NVRAM_SPACE;
+			if (copy_to_user(nvr->value, value, NVRAM_SPACE))
+				ret = -EFAULT;
+		}
+		kfree(value);
+	} else {
+		value = nvram_get(param);
+		if (value)
+		{
+			len = strlen(value) + 1;
+			if (nvr->len_value < len)
+				ret = -EOVERFLOW;
+			else if (copy_to_user(nvr->value, value, len))
+				ret = -EFAULT;
+			nvr->len_value = len;
+		}
+		else
+		{
+			nvr->len_value = -1;
+		}
 	}
-
-	if (copy_from_user(name, buf, count)) {
-		ret = -EFAULT;
-		goto done;
-	}
-
-	value = name;
-	name = strsep(&value, "=");
-
-	if (value)
-		ret = nvram_set(name, value) ? : count;
-	else
-		ret = nvram_unset(name) ? : count;
-
- done:
-	if (name != tmp)
-		kfree(name);
 
 	return ret;
 }
 
 static long dev_nvram_ioctl(struct file *file, unsigned int req, unsigned long arg)
 {
-	if (req != NVRAM_MAGIC)
-		return -EINVAL;
-
-	if(arg==0)
+	switch(req)
+	{
+	case NVRAM_IOCTL_COMMIT:
 		return nvram_commit();
-	else if(arg==1)
+	case NVRAM_IOCTL_CLEAR:
 		return nvram_clear();
+	case NVRAM_IOCTL_SET:
+		return user_nvram_set((anvram_ioctl_t __user *)arg);
+	case NVRAM_IOCTL_GET:
+		return user_nvram_get((anvram_ioctl_t __user *)arg);
+	}
 	
 	return -EINVAL;
-}
-
-static int
-dev_nvram_mmap(struct file *file, struct vm_area_struct *vma)
-{
-	unsigned long offset = virt_to_phys(nvram_buf);
-
-	int ret;
-	if ((ret = remap_pfn_range(vma, vma->vm_start, offset >> PAGE_SHIFT, vma->vm_end-vma->vm_start, vma->vm_page_prot)))
-	{
-		return -EAGAIN;
-	}
-
-	return 0;
 }
 
 static int
@@ -481,33 +500,22 @@ static struct file_operations dev_nvram_fops = {
 	owner:		THIS_MODULE,
 	open:		dev_nvram_open,
 	release:	dev_nvram_release,
-	read:		dev_nvram_read,
-	write:		dev_nvram_write,
 	unlocked_ioctl:	dev_nvram_ioctl,
-	mmap:		dev_nvram_mmap,
 };
 
 static void
 dev_nvram_exit(void)
 {
-	int order = 0;
-	struct page *page, *end;
+	if (g_pdentry != NULL) {
+		remove_proc_entry(MTD_NVRAM_NAME, NULL);
+	}
 
 	if (nvram_major >= 0)
 		unregister_chrdev(nvram_major, MTD_NVRAM_NAME);
 
-	if (g_pdentry != NULL)	{
-		remove_proc_entry(MTD_NVRAM_NAME, NULL);
-	}
-
-	if (nvram_buf) {
-		while ((PAGE_SIZE << order) < NVRAM_SPACE)
-			order++;
-		end = virt_to_page(nvram_buf + (PAGE_SIZE << order) - 1);
-		for (page = virt_to_page(nvram_buf); page <= end; page++)
-			mem_map_unreserve(page);
-	
-		kfree (nvram_buf);
+	if (nvram_values) {
+		kfree(nvram_values);
+		nvram_values = NULL;
 	}
 
 	_nvram_uninit();
@@ -516,28 +524,14 @@ dev_nvram_exit(void)
 static int __init
 dev_nvram_init(void)
 {
-	int order = 0, ret = 0;
-	struct page *page, *end;
+	int ret;
 
 	/* Initialize hash table lock */
 	spin_lock_init(&nvram_lock);
 
-	/* Initialize commit semaphore */
-	init_MUTEX(&nvram_sem);
-
-	nvram_buf = kmalloc(NVRAM_SPACE, GFP_ATOMIC);
-	if (!nvram_buf)
+	nvram_values = kzalloc(NVRAM_VALUES_SPACE, GFP_ATOMIC);
+	if (!nvram_values)
 		return -ENOMEM;
-	
-	memset(nvram_buf, 0, NVRAM_SPACE);
-
-	/* Allocate and reserve memory to mmap() */
-	while ((PAGE_SIZE << order) < NVRAM_SPACE)
-		order++;
-	end = virt_to_page(nvram_buf + (PAGE_SIZE << order) - 1);
-	for (page = virt_to_page(nvram_buf); page <= end; page++) {
-		mem_map_reserve(page);
-	}
 
 	/* Initialize hash table */
 	_nvram_init();
@@ -553,11 +547,9 @@ dev_nvram_init(void)
 	
 	g_pdentry = create_proc_read_entry(MTD_NVRAM_NAME, 0444, NULL, nvram_proc_version_read, NULL);
 	if (!g_pdentry) {
-		ret  = -ENOMEM;
+		ret = -ENOMEM;
 		goto err;
 	}
-
-	g_pdentry->owner = THIS_MODULE;
 
 	printk("ASUS NVRAM: initialized. Available NVRAM space: %d\n", NVRAM_SPACE);
 
@@ -568,7 +560,7 @@ err:
 	return ret;
 }
 
-module_init(dev_nvram_init);
+late_initcall(dev_nvram_init);
 module_exit(dev_nvram_exit);
 MODULE_LICENSE("GPL");
 MODULE_VERSION(NVRAM_DRIVER_VERSION);

trunk/linux-3.0.x/drivers/nvram/nvram_linux.c
@@ -31,11 +31,9 @@
 
 #include "nvram.c"
 
-
-#define MTD_NVRAM_NAME	"Config"
-
-#define mem_map_reserve(p)		set_bit(PG_reserved, &((p)->flags))
-#define mem_map_unreserve(p)		clear_bit(PG_reserved, &((p)->flags))
+#define NVRAM_DRIVER_VERSION	"0.04"
+#define MTD_NVRAM_NAME		"Config"
+#define NVRAM_VALUES_SPACE	(NVRAM_SPACE*2)
 
 extern int ra_mtd_read_nm(char *name, loff_t from, size_t len, u_char *buf);
 extern int ra_mtd_write_nm(char *name, loff_t to, size_t len, const u_char *buf);
@@ -45,7 +43,7 @@ static DEFINE_SPINLOCK(nvram_lock);
 static DEFINE_MUTEX(nvram_sem);
 static int nvram_major = -1;
 static struct proc_dir_entry *g_pdentry = NULL;
-static char *nvram_buf = NULL;
+static char *nvram_values = NULL;
 static unsigned long nvram_offset = 0;
 
 // from src/shared/bcmutils.c
@@ -122,8 +120,6 @@ hndcrc8(
 	return crc;
 }
 
-#define NVRAM_DRIVER_VERSION	"0.03"
-
 static int 
 nvram_proc_version_read(char *buf, char **start, off_t offset, int count, int *eof, void *data)
 {
@@ -163,27 +159,26 @@ _nvram_read_mtd(unsigned char *buf)
 struct nvram_tuple *
 _nvram_realloc(struct nvram_tuple *t, const char *name, const char *value)
 {
-	if ((nvram_offset + strlen(value) + 1) > NVRAM_SPACE) {
+	unsigned long val_len = (strlen(value) + 1);
+
+	if ((nvram_offset + val_len) >= NVRAM_VALUES_SPACE)
 		return NULL;
-	}
 
 	if (!t) {
 		if (!(t = kmalloc(sizeof(struct nvram_tuple) + strlen(name) + 1, GFP_ATOMIC))) {
 			return NULL;
 		}
-
 		/* Copy name */
 		t->name = (char *) &t[1];
 		strcpy(t->name, name);
-
 		t->value = NULL;
 	}
 
 	/* Copy value */
 	if (!t->value || strcmp(t->value, value)) {
-		t->value = &nvram_buf[nvram_offset];
+		t->value = &nvram_values[nvram_offset];
 		strcpy(t->value, value);
-		nvram_offset += strlen(value) + 1;
+		nvram_offset += val_len;
 	}
 
 	return t;
@@ -353,114 +348,138 @@ EXPORT_SYMBOL(nvram_commit);
 EXPORT_SYMBOL(nvram_clear);
 
 /* User mode interface below */
-
-static ssize_t
-dev_nvram_read(struct file *file, char *buf, size_t count, loff_t *ppos)
+int
+user_nvram_set(anvram_ioctl_t __user *nvr)
 {
-	char tmp[100], *name = tmp, *value;
-	ssize_t ret;
-	unsigned long off;
-	
-	if (count > sizeof(tmp)) {
-		if (!(name = kmalloc(count, GFP_KERNEL)))
+	int ret;
+	char param[NVRAM_MAX_PARAM_LEN];
+	char tmp[64], *value;
+
+	if (!nvr)
+		return -EINVAL;
+
+	if (nvr->size != sizeof(anvram_ioctl_t))
+		return -EINVAL;
+
+	if (nvr->len_param > (NVRAM_MAX_PARAM_LEN-1) || nvr->len_param < 0)
+		return -EOVERFLOW;
+
+	if (copy_from_user(param, nvr->param, nvr->len_param))
+		return -EFAULT;
+
+	param[nvr->len_param] = '\0';
+	if (!param[0])
+		return -EINVAL;
+
+	if (nvr->len_value > (NVRAM_MAX_VALUE_LEN-1) || nvr->len_value < 0)
+		return -EOVERFLOW;
+
+	value = tmp;
+	if ((nvr->len_value+1) > sizeof(tmp)) {
+		if (!(value = kmalloc(nvr->len_value+1, GFP_KERNEL)))
 			return -ENOMEM;
 	}
 
-	if (copy_from_user(name, buf, count)) {
-		ret = -EFAULT;
-		goto done;
-	}
-
-	if (*name == '\0') {
-		/* Get all variables */
-		ret = nvram_getall(name, count);
-		if (ret == 0) {
-			if (copy_to_user(buf, name, count)) {
-				ret = -EFAULT;
-				goto done;
-			}
-			ret = count;
-		}
-	} else {
-		if (!(value = nvram_get(name))) {
-			ret = 0;
-			goto done;
-		}
-
-		/* Provide the offset into mmap() space */
-		off = (unsigned long) value - (unsigned long) nvram_buf;
-
-		if (put_user(off, (unsigned long *) buf)) {
+	if (nvr->len_value > 0) {
+		if (copy_from_user(value, nvr->value, nvr->len_value)) {
 			ret = -EFAULT;
 			goto done;
 		}
-
-		ret = sizeof(unsigned long);
 	}
 
+	value[nvr->len_value] = '\0';
+
+	if (value[0])
+		ret = nvram_set(param, value);
+	else
+		ret = nvram_unset(param);
+
 done:
-	if (name != tmp)
-		kfree(name);
+	if (value != tmp)
+		kfree(value);
 
 	return ret;
 }
 
-static ssize_t
-dev_nvram_write(struct file *file, const char *buf, size_t count, loff_t *ppos)
+int
+user_nvram_get(anvram_ioctl_t __user *nvr)
 {
-	char tmp[100], *name = tmp, *value;
-	ssize_t ret;
+	int len, ret;
+	char param[NVRAM_MAX_PARAM_LEN];
+	char *value;
 
-	if (count > sizeof(tmp)) {
-		if (!(name = kmalloc(count, GFP_KERNEL)))
+	if (!nvr)
+		return -EINVAL;
+
+	if (nvr->size != sizeof(anvram_ioctl_t))
+		return -EINVAL;
+
+	if (nvr->len_value < 1)
+		return -EINVAL;
+
+	if (nvr->len_param > (NVRAM_MAX_PARAM_LEN-1) || nvr->len_param < 0)
+		return -EINVAL;
+
+	if (nvr->len_param > 0) {
+		if (copy_from_user(param, nvr->param, nvr->len_param))
+			return -EFAULT;
+	}
+
+	param[nvr->len_param] = '\0';
+
+	ret = 0;
+
+	if (param[0] == '\0') {
+		if (nvr->len_value < NVRAM_SPACE) {
+			nvr->len_value = NVRAM_SPACE;
+			return -EOVERFLOW;
+		}
+		
+		if (!(value = (char*)kmalloc(NVRAM_SPACE, GFP_KERNEL)))
 			return -ENOMEM;
+		
+		ret = nvram_getall(value, NVRAM_SPACE);
+		if (ret == 0) {
+			nvr->len_value = NVRAM_SPACE;
+			if (copy_to_user(nvr->value, value, NVRAM_SPACE))
+				ret = -EFAULT;
+		}
+		kfree(value);
+	} else {
+		value = nvram_get(param);
+		if (value)
+		{
+			len = strlen(value) + 1;
+			if (nvr->len_value < len)
+				ret = -EOVERFLOW;
+			else if (copy_to_user(nvr->value, value, len))
+				ret = -EFAULT;
+			nvr->len_value = len;
+		}
+		else
+		{
+			nvr->len_value = -1;
+		}
 	}
-
-	if (copy_from_user(name, buf, count)) {
-		ret = -EFAULT;
-		goto done;
-	}
-
-	value = name;
-	name = strsep(&value, "=");
-
-	if (value)
-		ret = nvram_set(name, value) ? : count;
-	else
-		ret = nvram_unset(name) ? : count;
-
- done:
-	if (name != tmp)
-		kfree(name);
 
 	return ret;
 }
 
 static long dev_nvram_ioctl(struct file *file, unsigned int req, unsigned long arg)
 {
-	if (req != NVRAM_MAGIC)
-		return -EINVAL;
-
-	if(arg==0)
+	switch(req)
+	{
+	case NVRAM_IOCTL_COMMIT:
 		return nvram_commit();
-	else if(arg==1)
+	case NVRAM_IOCTL_CLEAR:
 		return nvram_clear();
+	case NVRAM_IOCTL_SET:
+		return user_nvram_set((anvram_ioctl_t __user *)arg);
+	case NVRAM_IOCTL_GET:
+		return user_nvram_get((anvram_ioctl_t __user *)arg);
+	}
 	
 	return -EINVAL;
-}
-
-static int
-dev_nvram_mmap(struct file *file, struct vm_area_struct *vma)
-{
-	unsigned long offset = virt_to_phys(nvram_buf);
-
-	int ret;
-	if ((ret = remap_pfn_range(vma, vma->vm_start, offset >> PAGE_SHIFT, vma->vm_end-vma->vm_start, vma->vm_page_prot)))
-	{
-		return -EAGAIN;
-	}
-
-	return 0;
 }
 
 static int
@@ -481,33 +500,22 @@ static struct file_operations dev_nvram_fops = {
 	owner:		THIS_MODULE,
 	open:		dev_nvram_open,
 	release:	dev_nvram_release,
-	read:		dev_nvram_read,
-	write:		dev_nvram_write,
 	unlocked_ioctl:	dev_nvram_ioctl,
-	mmap:		dev_nvram_mmap,
 };
 
 static void
 dev_nvram_exit(void)
 {
-	int order = 0;
-	struct page *page, *end;
+	if (g_pdentry != NULL) {
+		remove_proc_entry(MTD_NVRAM_NAME, NULL);
+	}
 
 	if (nvram_major >= 0)
 		unregister_chrdev(nvram_major, MTD_NVRAM_NAME);
 
-	if (g_pdentry != NULL)	{
-		remove_proc_entry(MTD_NVRAM_NAME, NULL);
-	}
-
-	if (nvram_buf) {
-		while ((PAGE_SIZE << order) < NVRAM_SPACE)
-			order++;
-		end = virt_to_page(nvram_buf + (PAGE_SIZE << order) - 1);
-		for (page = virt_to_page(nvram_buf); page <= end; page++)
-			mem_map_unreserve(page);
-	
-		kfree (nvram_buf);
+	if (nvram_values) {
+		kfree(nvram_values);
+		nvram_values = NULL;
 	}
 
 	_nvram_uninit();
@@ -516,25 +524,14 @@ dev_nvram_exit(void)
 static int __init
 dev_nvram_init(void)
 {
-	int order = 0, ret = 0;
-	struct page *page, *end;
+	int ret;
 
 	/* Initialize hash table lock */
 	spin_lock_init(&nvram_lock);
 
-	nvram_buf = kmalloc(NVRAM_SPACE, GFP_ATOMIC);
-	if (!nvram_buf)
+	nvram_values = kzalloc(NVRAM_VALUES_SPACE, GFP_ATOMIC);
+	if (!nvram_values)
 		return -ENOMEM;
-	
-	memset(nvram_buf, 0, NVRAM_SPACE);
-
-	/* Allocate and reserve memory to mmap() */
-	while ((PAGE_SIZE << order) < NVRAM_SPACE)
-		order++;
-	end = virt_to_page(nvram_buf + (PAGE_SIZE << order) - 1);
-	for (page = virt_to_page(nvram_buf); page <= end; page++) {
-		mem_map_reserve(page);
-	}
 
 	/* Initialize hash table */
 	_nvram_init();
@@ -550,7 +547,7 @@ dev_nvram_init(void)
 	
 	g_pdentry = create_proc_read_entry(MTD_NVRAM_NAME, 0444, NULL, nvram_proc_version_read, NULL);
 	if (!g_pdentry) {
-		ret  = -ENOMEM;
+		ret = -ENOMEM;
 		goto err;
 	}
 

trunk/user/shared/nvram_linux.c
@@ -14,20 +14,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307 USA
  */
-/*
- * NVRAM variable manipulation (Linux user mode half)
- *
- * Copyright 2004, ASUSTeK Inc.
- * All Rights Reserved.
- * 
- * THIS SOFTWARE IS OFFERED "AS IS", AND BROADCOM GRANTS NO WARRANTIES OF ANY
- * KIND, EXPRESS OR IMPLIED, BY STATUTE, COMMUNICATION OR OTHERWISE. BROADCOM
- * SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A SPECIFIC PURPOSE OR NONINFRINGEMENT CONCERNING THIS SOFTWARE.
- *
- * $Id: nvram_linux.c,v 1.1 2007/06/12 02:28:08 arthur Exp $
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -38,96 +24,76 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <sys/mman.h>
 
 #include <nvram/bcmnvram.h>
 
-#define PATH_DEV_NVRAM "/dev/nvram"
-
-/* Globals */
-static int nvram_fd = -1;
-static char *nvram_buf = NULL;
-static char nvram_empty[4] = {0};
-
-int
-nvram_init(void)
-{
-	if ((nvram_fd = open(PATH_DEV_NVRAM, O_RDWR)) < 0)
-		goto err;
-
-	/* Map kernel string buffer into user space */
-	if ((nvram_buf = mmap(NULL, NVRAM_SPACE, PROT_READ, MAP_SHARED, nvram_fd, 0)) == MAP_FAILED) {
-		fprintf (stderr, "%s() mmap fail, return 0x%p\n", __FUNCTION__, nvram_buf); //eric++
-		close(nvram_fd);
-		nvram_fd = -1;
-		goto err;
-	}
-
-	return 0;
-
-err:
-	perror(PATH_DEV_NVRAM);
-	return errno;
-}
-
-void
-nvram_exit(void)
-{
-	if (nvram_fd) {
-		close(nvram_fd);
-		nvram_fd = -1;
-	}
-}
+#define PATH_DEV_NVRAM	"/dev/nvram"
+#define MAX_CACHE_LEN	(2*NVRAM_MAX_VALUE_LEN)
+#define MIN_CACHE_RES	32
 
 char *
 nvram_get_(const char *name)
 {
-	size_t count = strlen(name) + 1;
-	char tmp[NVRAM_MAX_PARAM_LEN], *value;
-	unsigned long *off = (unsigned long *) tmp;
+	static int  nvr_index = 0;
+	static char nvr_cache[MAX_CACHE_LEN] = {0};
+	int ret, i_cache, nvram_fd;
+	anvram_ioctl_t nvr;
 
-	if (nvram_fd < 0)
-		if (nvram_init())
-			return NULL;
-
-	if (count > sizeof(tmp)) {
-		if (!(off = malloc(count)))
-			return NULL;
+	nvram_fd = open(PATH_DEV_NVRAM, O_RDWR);
+	if (nvram_fd < 0) {
+		perror(PATH_DEV_NVRAM);
+		return NULL;
 	}
 
-	/* Get offset into mmap() space */
-	strcpy((char *) off, name);
-	count = read(nvram_fd, off, count);
+	i_cache = nvr_index;
+	if ((MAX_CACHE_LEN - i_cache) < MIN_CACHE_RES)
+		i_cache = 0;
 
-	if (count == sizeof(unsigned long))
-		value = &nvram_buf[*off];
+	nvr.size = sizeof(nvr);
+	nvr.len_param = strlen(name);
+	nvr.len_value = MAX_CACHE_LEN - i_cache;
+	nvr.param = (char*)name;
+	nvr.value = &nvr_cache[i_cache];
+
+	ret = ioctl(nvram_fd, NVRAM_IOCTL_GET, &nvr);
+	if (ret < 0) {
+		if (errno == EOVERFLOW && nvr.len_value > 0 && nvr.len_value <= MAX_CACHE_LEN) {
+			if ((MAX_CACHE_LEN - i_cache) < nvr.len_value)
+				i_cache = 0;
+			nvr.len_value = MAX_CACHE_LEN - i_cache;
+			nvr.value = &nvr_cache[i_cache];
+			ret = ioctl(nvram_fd, NVRAM_IOCTL_GET, &nvr);
+		}
+		if (ret < 0) {
+			perror(PATH_DEV_NVRAM);
+			close(nvram_fd);
+			return NULL;
+		}
+	}
+
+	if (nvr.len_value < 1)
+		nvr.value = NULL;
 	else
-		value = NULL;
+		nvr_index = i_cache + nvr.len_value;
 
-	if (count < 0)
-		perror(PATH_DEV_NVRAM);
+	close(nvram_fd);
 
-	if (off != (unsigned long *) tmp)
-		free(off);
-
-	return value;
+	return nvr.value;
 }
 
 char *
 nvram_get(const char *name)
 {
-	char *value = nvram_get_(name);
-	if (value > 0)
-		return value;
-	else
-		return NULL;
+	return nvram_get_(name);
 }
 
 char *
 nvram_safe_get(const char *name)
 {
+	static char nvram_empty[4] = {0};
+
 	char *value = nvram_get_(name);
-	if (value > 0)
+	if (value)
 		return value;
 	else
 		return nvram_empty;
@@ -137,7 +103,7 @@ int
 nvram_get_int(const char *name)
 {
 	char *value = nvram_get_(name);
-	if (value > 0)
+	if (value)
 		return atoi(value);
 	else
 		return 0;
@@ -146,58 +112,63 @@ nvram_get_int(const char *name)
 int
 nvram_getall(char *buf, int count)
 {
-	int ret;
+	int ret, nvram_fd;
+	anvram_ioctl_t nvr;
 
-	if (nvram_fd < 0)
-		if ((ret = nvram_init()))
-			return ret;
-	
-	if (!buf || count == 0)
+	if (!buf || count < 1)
 		return 0;
+
+	nvram_fd = open(PATH_DEV_NVRAM, O_RDWR);
+	if (nvram_fd < 0) {
+		perror(PATH_DEV_NVRAM);
+		return -1;
+	}
 
 	/* Get all variables */
 	*buf = '\0';
 
-	ret = read(nvram_fd, buf, count);
+	nvr.size = sizeof(nvr);
+	nvr.len_param = 0;
+	nvr.len_value = count;
+	nvr.param = NULL;
+	nvr.value = buf;
+
+	ret = ioctl(nvram_fd, NVRAM_IOCTL_GET, &nvr);
 	if (ret < 0)
 		perror(PATH_DEV_NVRAM);
 
-	return (ret == count) ? 0 : ret;
+	close(nvram_fd);
+
+	return ret;
 }
 
 int
 _nvram_set(const char *name, const char *value)
 {
-	size_t count = strlen(name) + 1;
-	char tmp[256], *buf = tmp;
-	int ret;
+	int ret, nvram_fd;
+	anvram_ioctl_t nvr;
 
-	if (nvram_fd < 0)
-		if ((ret = nvram_init()))
-			return ret;
-
-	/* Unset if value is NULL */
-	if (value)
-		count += strlen(value) + 1;
-
-	if (count > sizeof(tmp)) {
-		if (!(buf = malloc(count)))
-			return -ENOMEM;
+	nvram_fd = open(PATH_DEV_NVRAM, O_RDWR);
+	if (nvram_fd < 0) {
+		perror(PATH_DEV_NVRAM);
+		return -1;
 	}
 
+	nvr.size = sizeof(nvr);
+	nvr.len_param = strlen(name);
+	nvr.len_value = 0;
+	nvr.param = (char*)name;
+	nvr.value = (char*)value;
 	if (value)
-		sprintf(buf, "%s=%s", name, value);
-	else
-		strcpy(buf, name);
+		nvr.len_value = strlen(value);
 
-	ret = write(nvram_fd, buf, count);
+	ret = ioctl(nvram_fd, NVRAM_IOCTL_SET, &nvr);
 	if (ret < 0)
 		perror(PATH_DEV_NVRAM);
 
-	if (buf != tmp)
-		free(buf);
+	close(nvram_fd);
 
-	return (ret == count) ? 0 : ret;
+	return ret;
 }
 
 int
@@ -236,15 +207,19 @@ nvram_invmatch(const char *name, char *invmatch)
 int
 nvram_commit(void)
 {
-	int ret;
+	int ret, nvram_fd;
 
-	if (nvram_fd < 0)
-		if ((ret = nvram_init()))
-			return ret;
+	nvram_fd = open(PATH_DEV_NVRAM, O_RDWR);
+	if (nvram_fd < 0) {
+		perror(PATH_DEV_NVRAM);
+		return -1;
+	}
 
-	ret = ioctl(nvram_fd, NVRAM_MAGIC, 0);
+	ret = ioctl(nvram_fd, NVRAM_IOCTL_COMMIT, 0);
 	if (ret < 0)
 		perror(PATH_DEV_NVRAM);
+
+	close(nvram_fd);
 
 	return ret;
 }
@@ -252,15 +227,20 @@ nvram_commit(void)
 int
 nvram_clear(void)
 {
-	int ret;
+	int ret, nvram_fd;
 
-	if (nvram_fd < 0)
-		if ((ret = nvram_init()))
-			return ret;
+	nvram_fd = open(PATH_DEV_NVRAM, O_RDWR);
+	if (nvram_fd < 0) {
+		perror(PATH_DEV_NVRAM);
+		return -1;
+	}
 
-	ret = ioctl(nvram_fd, NVRAM_MAGIC, 1);
+	ret = ioctl(nvram_fd, NVRAM_IOCTL_CLEAR, 0);
 	if (ret < 0)
 		perror(PATH_DEV_NVRAM);
 
+	close(nvram_fd);
+
 	return ret;
 }
+

commit 671dc2ca28793c1255df25ae526444f260021ec1

nvram driver: fix unset regression from commit 7d23b547487b

trunk/linux-2.6.21.x/drivers/nvram/nvram_linux.c
@@ -31,7 +31,7 @@
 
 #include "nvram.c"
 
-#define NVRAM_DRIVER_VERSION	"0.04"
+#define NVRAM_DRIVER_VERSION	"0.05"
 #define MTD_NVRAM_NAME		"Config"
 #define NVRAM_VALUES_SPACE	(NVRAM_SPACE*2)
 
@@ -364,6 +364,9 @@ user_nvram_set(anvram_ioctl_t __user *nvr)
 	if (nvr->len_param > (NVRAM_MAX_PARAM_LEN-1) || nvr->len_param < 0)
 		return -EOVERFLOW;
 
+	if (!nvr->param)
+		return -EINVAL;
+
 	if (copy_from_user(param, nvr->param, nvr->len_param))
 		return -EFAULT;
 
@@ -380,16 +383,17 @@ user_nvram_set(anvram_ioctl_t __user *nvr)
 			return -ENOMEM;
 	}
 
-	if (nvr->len_value > 0) {
+	if (nvr->len_value > 0 && nvr->value) {
 		if (copy_from_user(value, nvr->value, nvr->len_value)) {
 			ret = -EFAULT;
 			goto done;
 		}
+		value[nvr->len_value] = '\0';
+	} else {
+		value[0] = '\0';
 	}
 
-	value[nvr->len_value] = '\0';
-
-	if (value[0])
+	if (nvr->value)
 		ret = nvram_set(param, value);
 	else
 		ret = nvram_unset(param);
@@ -420,12 +424,13 @@ user_nvram_get(anvram_ioctl_t __user *nvr)
 	if (nvr->len_param > (NVRAM_MAX_PARAM_LEN-1) || nvr->len_param < 0)
 		return -EINVAL;
 
-	if (nvr->len_param > 0) {
+	if (nvr->len_param > 0 && nvr->param) {
 		if (copy_from_user(param, nvr->param, nvr->len_param))
 			return -EFAULT;
+		param[nvr->len_param] = '\0';
+	} else {
+		param[0] = '\0';
 	}
-
-	param[nvr->len_param] = '\0';
 
 	ret = 0;
 

trunk/linux-3.0.x/drivers/nvram/nvram_linux.c
@@ -31,7 +31,7 @@
 
 #include "nvram.c"
 
-#define NVRAM_DRIVER_VERSION	"0.04"
+#define NVRAM_DRIVER_VERSION	"0.05"
 #define MTD_NVRAM_NAME		"Config"
 #define NVRAM_VALUES_SPACE	(NVRAM_SPACE*2)
 
@@ -364,6 +364,9 @@ user_nvram_set(anvram_ioctl_t __user *nvr)
 	if (nvr->len_param > (NVRAM_MAX_PARAM_LEN-1) || nvr->len_param < 0)
 		return -EOVERFLOW;
 
+	if (!nvr->param)
+		return -EINVAL;
+
 	if (copy_from_user(param, nvr->param, nvr->len_param))
 		return -EFAULT;
 
@@ -380,16 +383,17 @@ user_nvram_set(anvram_ioctl_t __user *nvr)
 			return -ENOMEM;
 	}
 
-	if (nvr->len_value > 0) {
+	if (nvr->len_value > 0 && nvr->value) {
 		if (copy_from_user(value, nvr->value, nvr->len_value)) {
 			ret = -EFAULT;
 			goto done;
 		}
+		value[nvr->len_value] = '\0';
+	} else {
+		value[0] = '\0';
 	}
 
-	value[nvr->len_value] = '\0';
-
-	if (value[0])
+	if (nvr->value)
 		ret = nvram_set(param, value);
 	else
 		ret = nvram_unset(param);
@@ -420,12 +424,13 @@ user_nvram_get(anvram_ioctl_t __user *nvr)
 	if (nvr->len_param > (NVRAM_MAX_PARAM_LEN-1) || nvr->len_param < 0)
 		return -EINVAL;
 
-	if (nvr->len_param > 0) {
+	if (nvr->len_param > 0 && nvr->param) {
 		if (copy_from_user(param, nvr->param, nvr->len_param))
 			return -EFAULT;
+		param[nvr->len_param] = '\0';
+	} else {
+		param[0] = '\0';
 	}
-
-	param[nvr->len_param] = '\0';
 
 	ret = 0;
 