tsl0922/padavan

commit fc19557a9e2d548e8982c7d4a161217a163f8d53

httpd,www: add dropdown menu for fast select DLNA path, fix output national resources from nvram to webui

trunk/user/httpd/web_ex.c
@@ -476,7 +476,6 @@ void websApply(webs_t wp, char_t *url)
 	websDone (wp, 200);
 }
 
-
 /*
  * Example: 
  * lan_ipaddr=192.168.1.1
@@ -486,81 +485,39 @@ void websApply(webs_t wp, char_t *url)
 static int
 ej_nvram_get_x(int eid, webs_t wp, int argc, char_t **argv)
 {
-	char *sid, *name, *c;
-	int ret = 0;
+	char *sid, *name, *cn, *c;
 
 	if (ejArgs(argc, argv, "%s %s", &sid, &name) < 2) {
 		websError(wp, 400, "Insufficient args\n");
 		return -1;
 	}
 
-	for (c = nvram_safe_get(name); *c; c++) {
-		if (isprint(*c) &&
-		    *c != '"' && *c != '&' && *c != '<' && *c != '>')
-			ret += websWrite(wp, "%c", *c);
+	cn = nvram_safe_get(name);
+	for (c = cn; *c; c++) {
+		if (*c == 0x23 || // #
+		    *c == 0x26 || // &
+		    *c == 0x3C || // <
+		    *c == 0x3E)   // >
+			*c = '_';
 		else
-			ret += websWrite(wp, "&#%d", *c);
+		if (*c == 0x22)   // "
+			*c = ' ';
 	}
 
-	return ret;
+	return websWrite(wp, "%s", cn);
 }
 
 static int
 ej_nvram_get_ddns(int eid, webs_t wp, int argc, char_t **argv)
 {
-	char *sid, *name, *c;
-	int ret = 0;
+	int ret = ej_nvram_get_x(eid, wp, argc, argv);
 
-	if (ejArgs(argc, argv, "%s %s", &sid, &name) < 2) {
-		websError(wp, 400, "Insufficient args\n");
-		return -1;
-	}
-
-	for (c = nvram_safe_get(name); *c; c++) {
-		if (isprint(*c) &&
-		    *c != '"' && *c != '&' && *c != '<' && *c != '>')
-			ret += websWrite(wp, "%c", *c);
-		else
-			ret += websWrite(wp, "&#%d", *c);
-	}
-
-	if (strcmp(name,"ddns_return_code")==0) {
-		if (!nvram_match("ddns_return_code", "ddns_query")) {
-			nvram_set_temp("ddns_return_code","");
-		}
-	}
+	if (!nvram_match("ddns_return_code", "ddns_query"))
+		nvram_set_temp("ddns_return_code", "");
 
 	return ret;
 }
 
-/*
- * Example: 
- * lan_ipaddr=192.168.1.1
- * <% nvram_get_x("lan_ipaddr"); %> produces "192.168.1.1"
- * <% nvram_get_x("undefined"); %> produces ""
- */
-static int
-ej_nvram_get_f(int eid, webs_t wp, int argc, char_t **argv)
-{
-	char *file, *field, *c, buf[64];
-	int ret = 0;
-
-	if (ejArgs(argc, argv, "%s %s", &file, &field) < 2) {
-		websError(wp, 400, "Insufficient args\n");
-		return -1;
-	}
-	
-	strcpy(buf, nvram_safe_get(field));
-	for (c = buf; *c; c++) {
-		if (isprint(*c) &&
-		    *c != '"' && *c != '&' && *c != '<' && *c != '>')
-			ret += websWrite(wp, "%c", *c);
-		else
-			ret += websWrite(wp, "&#%d", *c);
-	}
-
-	return ret;
-}
 
 /*
  * Example: 
@@ -4359,7 +4316,6 @@ do_log_cgi(char *url, FILE *stream)
 	fputs("\r\n", stream); /* terminator */
 }
 
-//2008.08 magic{
 struct mime_handler mime_handlers[] = {
 	{ "Nologin.asp", "text/html", no_cache_IE9, do_html_post_and_get, do_ej, NULL },
 	{ "jquery.js", "text/javascript", cache_static, NULL, do_file, NULL }, // 2012.06 Eagle23
@@ -4408,9 +4364,39 @@ struct mime_handler mime_handlers[] = {
 
 	{ NULL, NULL, NULL, NULL, NULL, NULL }
 };
-//2008.08 magic}
 
-int ej_get_AiDisk_status(int eid, webs_t wp, int argc, char **argv) {
+static int ej_get_usb_share_list(int eid, webs_t wp, int argc, char **argv)
+{
+	disk_info_t *disks_info, *follow_disk;
+	partition_info_t *follow_partition;
+	int first_pool, ret;
+
+	disks_info = read_disk_data();
+	if (!disks_info)
+		return 0;
+
+	ret = 0;
+	first_pool = 1;
+	for (follow_disk = disks_info; follow_disk != NULL; follow_disk = follow_disk->next)
+		for (follow_partition = follow_disk->partitions; follow_partition != NULL; follow_partition = follow_partition->next) {
+			if (follow_partition->mount_point != NULL && strlen(follow_partition->mount_point) > 1) {
+				if (first_pool == 1)
+					first_pool = 0;
+				else
+					ret += websWrite(wp, ", ");
+				
+				ret += websWrite(wp, "[\"%s\",\"%s\",\"%s\"]", 
+					follow_partition->device, follow_partition->mount_point, follow_partition->file_system);
+			}
+		}
+
+	free_disk_data(disks_info);
+
+	return ret;
+}
+
+static int ej_get_AiDisk_status(int eid, webs_t wp, int argc, char **argv)
+{
 	disk_info_t *disks_info, *follow_disk;
 	partition_info_t *follow_partition;
 	char *follow_info;
@@ -4488,7 +4474,8 @@ int ej_get_AiDisk_status(int eid, webs_t wp, int argc, char **argv) {
 	return 0;
 }
 
-int ej_get_all_accounts(int eid, webs_t wp, int argc, char **argv) {
+static int ej_get_all_accounts(int eid, webs_t wp, int argc, char **argv)
+{
 	int acc_num = 0;
 	char **account_list = NULL;
 	char *acc_mode = "ftp";
@@ -4516,17 +4503,17 @@ int ej_get_all_accounts(int eid, webs_t wp, int argc, char **argv) {
 	return 0;
 }
 
-void start_flash_usbled()
+static void start_flash_usbled()
 {
 	doSystem("killall %s %s", "-SIGUSR1", "detect_link");
 }
 
-void stop_flash_usbled()
+static void stop_flash_usbled()
 {
 	doSystem("killall %s %s", "-SIGUSR2", "detect_link");
 }
 
-static int ej_safely_remove_disk(int eid, webs_t wp, int argc, char_t **argv) 
+static int ej_safely_remove_disk(int eid, webs_t wp, int argc, char_t **argv)
 {
 	int result;
 	int port_num = 0;
@@ -5989,7 +5976,6 @@ ej_select_list(int eid, webs_t wp, int argc, char_t **argv)
 
 struct ej_handler ej_handlers[] = {
 	{ "nvram_get_x", ej_nvram_get_x},
-	{ "nvram_get_f", ej_nvram_get_f},
 	{ "nvram_get_list_x", ej_nvram_get_list_x},
 	{ "nvram_get_buf_x", ej_nvram_get_buf_x},
 	{ "nvram_get_table_x", ej_nvram_get_table_x},
@@ -6041,6 +6027,7 @@ struct ej_handler ej_handlers[] = {
 	{ "disk_pool_mapping_info", ej_disk_pool_mapping_info},
 	{ "available_disk_names_and_sizes", ej_available_disk_names_and_sizes},
 	{ "get_usb_ports_info", ej_get_usb_ports_info},
+	{ "get_usb_share_list", ej_get_usb_share_list},
 	{ "get_AiDisk_status", ej_get_AiDisk_status},
 	{ "set_AiDisk_status", ej_set_AiDisk_status},
 	{ "get_all_accounts", ej_get_all_accounts},

trunk/user/libdisk/disk_io_tools.c
@@ -33,14 +33,14 @@ void sanity_name(char *name)
 
 	for (i = 0; i < len; i++)
 	{
-		if (name[i] == 0x22 ||
-		    name[i] == 0x23 ||
-		    name[i] == 0x25 ||
-		    name[i] == 0x27 ||
-		    name[i] == 0x2F ||
-		    name[i] == 0x5C ||
-		    name[i] == 0x60 ||
-		    name[i] == 0x84)
+		if (name[i] == 0x22 || // "
+		    name[i] == 0x23 || // #
+		    name[i] == 0x25 || // %
+		    name[i] == 0x27 || // '
+		    name[i] == 0x2F || // /
+		    name[i] == 0x5C || // \
+		    name[i] == 0x60 || // `
+		    name[i] == 0x84)   // „
 			name[i] = 0x20;
 	}
 }

trunk/user/www/n56u_ribbon_fixed/Advanced_ACL2g_Content.asp
@@ -37,7 +37,6 @@ var clients_info = getclients(1);
 
 var ACLList = [<% get_nvram_list("DeviceSecurity11b", "rt_ACLList"); %>];
 
-var over_var = 0;
 var isMenuopen = 0;
 
 function initial(){
@@ -100,7 +99,6 @@ function setClientMAC(num){
 	document.form.rt_maclist_x_0.value = clients_info[num][2];
 	document.form.rt_macdesc_x_0.value = clients_info[num][0];
 	hideClients_Block();
-	over_var = 0;
 }
 
 function showLANIPList(){
@@ -117,7 +115,7 @@ function showLANIPList(){
 			show_name = clients_info[i][0];
 		
 		if(clients_info[i][2]){
-			code += '<a href="javascript:void(0)"><div onmouseover="over_var=1;" onmouseout="over_var=0;" onclick="setClientMAC('+i+');"><strong>'+clients_info[i][1]+'</strong>';
+			code += '<a href="javascript:void(0)"><div onclick="setClientMAC('+i+');"><strong>'+clients_info[i][1]+'</strong>';
 			code += ' ['+clients_info[i][2]+']';
 			if(show_name && show_name.length > 0)
 				code += ' ('+show_name+')';
@@ -226,35 +224,6 @@ function done_validating(action){
 }
 
 </script>
-<style>
-#ClientList_Block{
-    width: 380px;
-    margin-top: 28px;
-    position:absolute;
-    text-align:left;
-    height:auto;
-    overflow-y:auto;
-    padding: 1px;
-    display:none;
-}
-#ClientList_Block div{
-    height:20px;
-    line-height:20px;
-    text-decoration:none;
-    padding-left:2px;
-}
-#ClientList_Block a{
-    color:#000;
-    font-size:12px;
-    text-decoration:none;
-}
-#ClientList_Block div:hover, #ClientList_Block a:hover{
-    cursor:default;
-    color: #005580;
-}
-    .input-append{margin-bottom: 0px;}
-    .input-append input{border-radius: 3px 0 0 3px;}
-</style>
 </head>
 
 <body onload="initial();">
@@ -339,10 +308,10 @@ function done_validating(action){
                                         </tr>
                                         <tr>
                                             <td width="35%">
-                                                <div id="ClientList_Block" class="alert alert-info"></div>
+                                                <div id="ClientList_Block" class="alert alert-info ddown-list" style="width: 400px;"></div>
                                                 <div class="input-append">
                                                     <input type="text" maxlength="12" class="span12" size="12" name="rt_maclist_x_0" onKeyPress="return is_hwaddr()" style="float:left; width: 175px"/>
-                                                    <button class="btn" id="chevron" style="border-radius: 0px 4px 4px 0px;" type="button" onclick="pullLANIPList(this);" title="Select the MAC of WiFi clients." onmouseover="over_var=1;" onmouseout="over_var=0;"><i class="icon icon-chevron-down"></i></button>
+                                                    <button class="btn btn-chevron" id="chevron" type="button" onclick="pullLANIPList(this);" title="Select the MAC of WiFi clients"><i class="icon icon-chevron-down"></i></button>
                                                 </div>
                                             </td>
                                             <td width="60%">

trunk/user/www/n56u_ribbon_fixed/Advanced_ACL_Content.asp
@@ -37,9 +37,7 @@ var clients_info = getclients(1);
 
 var ACLList = [<% get_nvram_list("DeviceSecurity11a", "ACLList"); %>];
 
-var over_var = 0;
 var isMenuopen = 0;
-
 
 function initial(){
 	show_banner(1);
@@ -100,7 +98,6 @@ function setClientMAC(num){
 	document.form.wl_maclist_x_0.value = clients_info[num][2];
 	document.form.wl_macdesc_x_0.value = clients_info[num][0];
 	hideClients_Block();
-	over_var = 0;
 }
 
 function showLANIPList(){
@@ -117,7 +114,7 @@ function showLANIPList(){
 			show_name = clients_info[i][0];
 		
 		if(clients_info[i][2]){
-			code += '<a href="javascript:void(0)"><div onmouseover="over_var=1;" onmouseout="over_var=0;" onclick="setClientMAC('+i+');"><strong>'+clients_info[i][1]+'</strong>';
+			code += '<a href="javascript:void(0)"><div onclick="setClientMAC('+i+');"><strong>'+clients_info[i][1]+'</strong>';
 			code += ' ['+clients_info[i][2]+']';
 			if(show_name && show_name.length > 0)
 				code += ' ('+show_name+')';
@@ -226,35 +223,6 @@ function done_validating(action){
 }
 
 </script>
-<style>
-#ClientList_Block{
-    width: 380px;
-    margin-top: 28px;
-    position:absolute;
-    text-align:left;
-    height:auto;
-    overflow-y:auto;
-    padding: 1px;
-    display:none;
-}
-#ClientList_Block div{
-    height:20px;
-    line-height:20px;
-    text-decoration:none;
-    padding-left:2px;
-}
-#ClientList_Block a{
-    color:#000;
-    font-size:12px;
-    text-decoration:none;
-}
-#ClientList_Block div:hover, #ClientList_Block a:hover{
-    cursor:default;
-    color: #005580;
-}
-    .input-append{margin-bottom: 0px;}
-    .input-append input{border-radius: 3px 0 0 3px;}
-</style>
 </head>
 
 <body onload="initial();">
@@ -337,10 +305,10 @@ function done_validating(action){
                                         </tr>
                                         <tr>
                                             <td width="35%">
-                                                <div id="ClientList_Block" class="alert alert-info"></div>
+                                                <div id="ClientList_Block" class="alert alert-info ddown-list" style="width: 400px;"></div>
                                                 <div class="input-append">
                                                     <input type="text" maxlength="12" class="span12" size="12" name="wl_maclist_x_0" onKeyPress="return is_hwaddr()" style="float:left; width: 175px"/>
-                                                    <button class="btn" id="chevron" style="border-radius: 0px 4px 4px 0px;" type="button" onclick="pullLANIPList(this);" title="Select the MAC of WiFi clients." onmouseover="over_var=1;" onmouseout="over_var=0;"><i class="icon icon-chevron-down"></i></button>
+                                                    <button class="btn btn-chevron" id="chevron" type="button" onclick="pullLANIPList(this);" title="Select the MAC of WiFi clients"><i class="icon icon-chevron-down"></i></button>
                                                 </div>
                                             </td>
                                             <td width="60%">

trunk/user/www/n56u_ribbon_fixed/Advanced_ASUSDDNS_Content.asp
@@ -110,8 +110,8 @@ function ddns_load_body(){
 	valid_wan_ip();
 
 	var ddns_hostname_x = '<% nvram_get_x("LANHostConfig","ddns_hostname_x"); %>';
-	var ddns_return_code = '<% nvram_get_ddns("LANHostConfig","ddns_return_code"); %>';
-	var ddns_server_x = '<% nvram_get_x("LANHostConfig","ddns_server_x"); %>';
+	var ddns_return_code = '<% nvram_get_ddns("","ddns_return_code"); %>';
+	var ddns_server_x = '<% nvram_get_x("","ddns_server_x"); %>';
 	var wan_ipaddr_t='<% nvram_get_x("","wan_ipaddr_t"); %>';
 
 	if (ddns_server_x == "")
@@ -435,7 +435,7 @@ function checkDDNSReturnCode(){
                                         <tr>
                                             <th><#LANHostConfig_x_DDNSStatus_itemname#></th>
                                             <td>
-                                                <input type="hidden" maxlength="15" class="button" size="12" name="" value="<% nvram_get_f("ddns.log","DDNSStatus"); %>">
+                                                <input type="hidden" maxlength="15" class="button" size="12" name="" value="<% nvram_get_x("", "DDNSStatus"); %>">
                                                 <input type="submit" maxlength="15" class="btn btn-info span12" style="max-width: 220px;" onclick="showLoading();return onSubmitApply('ddnsclient');" size="12" name="LANHostConfig_x_DDNSStatus_button" value="<#LANHostConfig_x_DDNSStatus_buttonname#>" />
                                             </td>
                                         </tr>

trunk/user/www/n56u_ribbon_fixed/Advanced_AiDisk_ftp.asp
@@ -594,7 +594,7 @@ function unload_body(){
                                                 <table cellspacing="0" cellpadding="0" class="table t-border-top-none" style="margin-bottom: 0px; border-top: 0 none;">
                                                     <tr>
                                                         <td width="175" align="left" style="padding-left: 0px;">
-                                                            <div class="machineName"><span><% nvram_get_f("general.log","productid"); %></span></div>
+                                                            <div class="machineName"><span><% nvram_get_x("", "productid"); %></span></div>
                                                         </td>
                                                         <td>
                                                             <div id="permissionTitle"></div>

trunk/user/www/n56u_ribbon_fixed/Advanced_AiDisk_others.asp
@@ -156,68 +156,75 @@ var ddns_enable = '<% nvram_get_x("LANHostConfig", "ddns_enable_x"); %>';
 var ddns_server = '<% nvram_get_x("LANHostConfig", "ddns_server_x"); %>';
 var ddns_hostname = '<% nvram_get_x("LANHostConfig", "ddns_hostname_x"); %>';
 
+var usb_share_list = [<% get_usb_share_list(); %>];
+var menu_open = [0, 0, 0];
+
 function initial(){
-    show_banner(1);
-    show_menu(5, 7, 1);
-    show_footer();
+	show_banner(1);
+	show_menu(5, 7, 1);
+	show_footer();
 
-    if(!found_utl_hdparm()){
-        $("row_spd").style.display = "none";
-        $("row_apm").style.display = "none";
-    }
+	if(!found_utl_hdparm()){
+		$("row_spd").style.display = "none";
+		$("row_apm").style.display = "none";
+	}
 
-    if(support_pcie_usb3()){
-        $("row_pcie_aspm").style.display = "";
-    }
+	if(support_pcie_usb3()){
+		$("row_pcie_aspm").style.display = "";
+	}
 
-    if(!found_app_smbd() && !found_app_ftpd()){
-        $("row_max_user").style.display = "none";
-    }
+	if(!found_app_smbd() && !found_app_ftpd()){
+		$("row_max_user").style.display = "none";
+	}
 
-    if(found_app_smbd()){
-        $("tbl_smbd").style.display = "";
-        change_smb_enabled();
-    }
+	if(found_app_smbd()){
+		$("tbl_smbd").style.display = "";
+		change_smb_enabled();
+	}
 
-    if(found_app_ftpd()){
-        $("tbl_ftpd").style.display = "";
-        change_ftp_enabled();
-    }
+	if(found_app_ftpd()){
+		$("tbl_ftpd").style.display = "";
+		change_ftp_enabled();
+	}
 
-    if(found_app_nfsd()){
-        $("tbl_nfsd").style.display = "";
-    }
+	if(found_app_nfsd()){
+		$("tbl_nfsd").style.display = "";
+	}
 
-    if(found_app_dlna()){
-        $("tbl_minidlna").style.display = "";
-        change_dms_enabled();
-    }
+	if(found_app_dlna()){
+		$("tbl_minidlna").style.display = "";
+		change_dms_enabled();
+	}
 
-    if(found_app_ffly()){
-        $("tbl_itunes").style.display = "";
-    }
+	if(found_app_ffly()){
+		$("tbl_itunes").style.display = "";
+	}
 
-    if(found_app_torr()){
-        $("tbl_trmd").style.display = "";
-        change_trmd_enabled();
-    }
+	if(found_app_torr()){
+		$("tbl_trmd").style.display = "";
+		change_trmd_enabled();
+	}
 
-    if(found_app_aria()){
-        $("tbl_aria").style.display = "";
-        change_aria_enabled();
-    }
+	if(found_app_aria()){
+		$("tbl_aria").style.display = "";
+		change_aria_enabled();
+	}
 
-    if (!document.form.apps_dms[0].checked){
-        $("web_dms_link").style.display = "none";
-    }
+	if (!document.form.apps_dms[0].checked){
+		$("web_dms_link").style.display = "none";
+	}
 
-    if (!document.form.apps_itunes[0].checked){
-        $("web_ffly_link").style.display = "none";
-    }
+	if (!document.form.apps_itunes[0].checked){
+		$("web_ffly_link").style.display = "none";
+	}
 
-    if (!document.form.trmd_enable[0].checked){
-        $("web_rpc_link").style.display = "none";
-    }
+	if (!document.form.trmd_enable[0].checked){
+		$("web_rpc_link").style.display = "none";
+	}
+
+	show_usb_share_list(0);
+	show_usb_share_list(1);
+	show_usb_share_list(2);
 }
 
 var window_rpc;
@@ -226,21 +233,107 @@ var window_ffly;
 var window_params="toolbar=yes,location=yes,directories=no,status=yes,menubar=yes,scrollbars=yes,resizable=yes,copyhistory=no,width=800,height=600";
 
 function on_rpc_link(){
-    var rpc_url="http://" + lan_ipaddr + ":" + document.form.trmd_rport.value;
-    window_rpc = window.open(rpc_url, "Transmission", window_params);
-    window_rpc.focus();
+	var rpc_url="http://" + lan_ipaddr + ":" + document.form.trmd_rport.value;
+	window_rpc = window.open(rpc_url, "Transmission", window_params);
+	window_rpc.focus();
 }
 
 function on_dms_link(){
-    var dms_url="http://" + lan_ipaddr + ":8200";
-    window_dms = window.open(dms_url, "Minidlna", window_params);
-    window_dms.focus();
+	var dms_url="http://" + lan_ipaddr + ":8200";
+	window_dms = window.open(dms_url, "Minidlna", window_params);
+	window_dms.focus();
 }
 
 function on_ffly_link(){
-    var ffly_url="http://" + lan_ipaddr + ":3689";
-    window_ffly = window.open(ffly_url, "Firefly", window_params);
-    window_ffly.focus();
+	var ffly_url="http://" + lan_ipaddr + ":3689";
+	window_ffly = window.open(ffly_url, "Firefly", window_params);
+	window_ffly.focus();
+}
+
+function hide_usb_share_list(idx){
+	var obj, obj3;
+	if (idx == 0) {
+		obj = $j("#chevron1");
+		obj3 = $("share_list1");
+	} else if (idx == 1) {
+		obj = $j("#chevron2");
+		obj3 = $("share_list2");
+	} else {
+		obj = $j("#chevron3");
+		obj3 = $("share_list3");
+	}
+	obj.children('i').removeClass('icon-chevron-up').addClass('icon-chevron-down');
+	obj3.style.display = "none";
+	menu_open[idx] = 0;
+}
+
+function set_usb_share(num, idx){
+	var path = usb_share_list[num][1];
+	if (idx == 0)
+		document.form.dlna_src1.value = "A,"+path;
+	else if (idx == 1)
+		document.form.dlna_src2.value = "V,"+path;
+	else
+		document.form.dlna_src3.value = "P,"+path;
+	hide_usb_share_list(idx);
+}
+
+function show_usb_share_list(idx){
+	var code = "";
+
+	for(var i = 0; i < usb_share_list.length; i++){
+		if (usb_share_list[i][1]) {
+			code += '<a href="javascript:void(0)"><div onclick="set_usb_share('+i+','+idx+');"><strong>'+usb_share_list[i][1]+'</strong>';
+			code += ' ['+usb_share_list[i][0]+']';
+			if (usb_share_list[i][2])
+				code += ' ('+usb_share_list[i][2]+')';
+			code += '</div></a>';
+		}
+	}
+
+	if (code == "")
+		code = '<div style="text-align: center;" onclick="hide_usb_share_list('+idx+');"><#Nodata#></div>';
+	code +='<!--[if lte IE 6.5]><iframe class="hackiframe2"></iframe><![endif]-->';
+
+	if (idx == 0) {
+		$("share_list1").innerHTML = code;
+	} else if (idx == 1) {
+		$("share_list2").innerHTML = code;
+	} else {
+		$("share_list3").innerHTML = code;
+	}
+}
+
+function pull_usb_share_list(obj,idx){
+	var obj2, obj3, idx2, idx3;
+	if(menu_open[idx] == 0){
+		$j(obj).children('i').removeClass('icon-chevron-down').addClass('icon-chevron-up');
+		if (idx == 0) {
+			obj2 = document.form.dlna_src1;
+			obj3 = $("share_list1");
+			idx2 = 1;
+			idx3 = 2;
+		} else if (idx == 1) {
+			obj2 = document.form.dlna_src2;
+			obj3 = $("share_list2");
+			idx2 = 0;
+			idx3 = 2;
+		} else {
+			obj2 = document.form.dlna_src3;
+			obj3 = $("share_list3");
+			idx2 = 0;
+			idx3 = 1;
+		}
+		if (menu_open[idx2])
+			hide_usb_share_list(idx2);
+		if (menu_open[idx3])
+			hide_usb_share_list(idx3);
+		obj2.focus();
+		obj3.style.display = "block";
+		menu_open[idx] = 1;
+	}
+	else
+		hide_usb_share_list(idx);
 }
 
 function change_smb_enabled(){
@@ -339,10 +432,10 @@ function done_validating(action){
 }
 </script>
 <style>
-    .nav-tabs > li > a {
-          padding-right: 6px;
-          padding-left: 6px;
-    }
+.nav-tabs > li > a {
+	padding-right: 6px;
+	padding-left: 6px;
+}
 </style>
 </head>
 
@@ -458,9 +551,9 @@ function done_validating(action){
                                             <th><#StorageAllowOptw#></th>
                                             <td>
                                                 <select name="optw_enable" class="input">
-                                                        <option value="0" <% nvram_match_x("", "optw_enable", "0", "selected"); %>><#checkbox_No#></option>
-                                                        <option value="1" <% nvram_match_x("", "optw_enable", "1", "selected"); %>>Optware (legacy)</option>
-                                                        <option value="2" <% nvram_match_x("", "optw_enable", "2", "selected"); %>>Entware</option>
+                                                    <option value="0" <% nvram_match_x("", "optw_enable", "0", "selected"); %>><#checkbox_No#></option>
+                                                    <option value="1" <% nvram_match_x("", "optw_enable", "1", "selected"); %>>Optware (legacy)</option>
+                                                    <option value="2" <% nvram_match_x("", "optw_enable", "2", "selected"); %>>Entware</option>
                                                 </select>
                                             </td>
                                         </tr>
@@ -631,7 +724,7 @@ function done_validating(action){
                                             <th>
                                                 <#StorageRootDLNA#>
                                             </th>
-                                            <td>
+                                            <td colspan="2">
                                                 <select name="dlna_root" class="input">
                                                     <option value="0" <% nvram_match_x("", "dlna_root", "0", "selected"); %>>Default</option>
                                                     <option value="1" <% nvram_match_x("", "dlna_root", "1", "selected"); %>>Browse Folders</option>
@@ -646,7 +739,11 @@ function done_validating(action){
                                                 <#StorageSourceDLNA#>
                                             </th>
                                             <td colspan="2">
-                                                <input type="text" name="dlna_src1" class="input" maxlength="255" size="32" value="<% nvram_get_x("", "dlna_src1"); %>"/>
+                                                <div id="share_list1" class="alert alert-info ddown-list"></div>
+                                                <div class="input-append">
+                                                    <input type="text" name="dlna_src1" class="input" maxlength="255" size="32" value="<% nvram_get_x("", "dlna_src1"); %>" style="float:left; width: 260px"/>
+                                                    <button class="btn btn-chevron" id="chevron1" type="button" onclick="pull_usb_share_list(this, 0);" title="Select share for audio"><i class="icon icon-chevron-down"></i></button>
+                                                </div>
                                             </td>
                                         </tr>
                                         <tr id="row_dms_src2">
@@ -654,7 +751,11 @@ function done_validating(action){
                                                 <#StorageSourceDLNA#>
                                             </th>
                                             <td colspan="2">
-                                                <input type="text" name="dlna_src2" class="input" maxlength="255" size="32" value="<% nvram_get_x("", "dlna_src2"); %>"/>
+                                                <div id="share_list2" class="alert alert-info ddown-list"></div>
+                                                <div class="input-append">
+                                                    <input type="text" name="dlna_src2" class="input" maxlength="255" size="32" value="<% nvram_get_x("", "dlna_src2"); %>" style="float:left; width: 260px"/>
+                                                    <button class="btn btn-chevron" id="chevron2" type="button" onclick="pull_usb_share_list(this, 1);" title="Select share for video"><i class="icon icon-chevron-down"></i></button>
+                                                </div>
                                             </td>
                                         </tr>
                                         <tr id="row_dms_src3">
@@ -662,7 +763,11 @@ function done_validating(action){
                                                 <#StorageSourceDLNA#>
                                             </th>
                                             <td colspan="2">
-                                                <input type="text" name="dlna_src3" class="input" maxlength="255" size="32" value="<% nvram_get_x("", "dlna_src3"); %>"/>
+                                                <div id="share_list3" class="alert alert-info ddown-list"></div>
+                                                <div class="input-append">
+                                                    <input type="text" name="dlna_src3" class="input" maxlength="255" size="32" value="<% nvram_get_x("", "dlna_src3"); %>" style="float:left; width: 260px"/>
+                                                    <button class="btn btn-chevron" id="chevron3" type="button" onclick="pull_usb_share_list(this, 2);" title="Select share for pictures"><i class="icon icon-chevron-down"></i></button>
+                                                </div>
                                             </td>
                                         </tr>
                                         <tr id="row_dms_dnew">

trunk/user/www/n56u_ribbon_fixed/Advanced_AiDisk_samba.asp
@@ -594,7 +594,7 @@ function unload_body(){
                                                 <table cellspacing="0" cellpadding="0" class="table t-border-top-none" style="margin-bottom: 0px; border-top: 0 none;">
                                                     <tr>
                                                         <td width="175" align="left" style="padding-left: 0px;">
-                                                            <div class="machineName"><span><% nvram_get_f("general.log","productid"); %></span></div>
+                                                            <div class="machineName"><span><% nvram_get_x("", "productid"); %></span></div>
                                                         </td>
                                                         <td>
                                                             <div id="permissionTitle"></div>

commit 8b241161d19f8cf729dbe159874b3ea06bb9d11d

httpd: fix RAS modem call number regression in commit fc19557a9e2d

trunk/user/httpd/web_ex.c
@@ -494,8 +494,7 @@ ej_nvram_get_x(int eid, webs_t wp, int argc, char_t **argv)
 
 	cn = nvram_safe_get(name);
 	for (c = cn; *c; c++) {
-		if (*c == 0x23 || // #
-		    *c == 0x26 || // &
+		if (*c == 0x26 || // &
 		    *c == 0x3C || // <
 		    *c == 0x3E)   // >
 			*c = '_';