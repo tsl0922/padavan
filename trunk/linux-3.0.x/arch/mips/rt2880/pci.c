tsl0922/padavan

commit d05385b7dc12a04670cdc1af44c602fdfaa74661

ralink arch: fix reset MT7621 PCIe peripherals on disable GPIO_PERST def

trunk/linux-3.0.x/arch/mips/rt2880/pci.c
@@ -614,13 +614,13 @@ int __init init_ralink_pci(void)
 #if defined (CONFIG_PCIE_PORT2)
 	val |= (0x1<<GPIO_PCIE_PORT2);
 #endif
-	RALINK_GPIO_CTRL0 |= val;			// switch PERST_N pin to output mode
-	mdelay(10);
+	mdelay(50);
 	RALINK_GPIO_DATA0 &= ~(val);			// fall PERST_N pin (reset peripherals)
-	mdelay(100);					// wait 100 ms pulse
+	RALINK_GPIO_CTRL0 |= val;			// switch PERST_N pin to output mode
 #else /* !defined (GPIO_PERST) */
-	RALINK_GPIOMODE &= ~(0x3<<PCIE_SHARE_PIN_SW);
+	RALINK_GPIOMODE &= ~(0x3<<PCIE_SHARE_PIN_SW);	// fall PERST_N pin (reset peripherals)
 #endif
+	mdelay(100);					// wait 100 ms pulse
 
 	val = 0;
 #if defined (CONFIG_PCIE_PORT0)

trunk/linux-3.4.x/arch/mips/rt2880/pci.c
@@ -614,13 +614,13 @@ int __init init_ralink_pci(void)
 #if defined (CONFIG_PCIE_PORT2)
 	val |= (0x1<<GPIO_PCIE_PORT2);
 #endif
-	RALINK_GPIO_CTRL0 |= val;			// switch PERST_N pin to output mode
-	mdelay(10);
+	mdelay(50);
 	RALINK_GPIO_DATA0 &= ~(val);			// fall PERST_N pin (reset peripherals)
-	mdelay(100);					// wait 100 ms pulse
+	RALINK_GPIO_CTRL0 |= val;			// switch PERST_N pin to output mode
 #else /* !defined (GPIO_PERST) */
-	RALINK_GPIOMODE &= ~(0x3<<PCIE_SHARE_PIN_SW);
+	RALINK_GPIOMODE &= ~(0x3<<PCIE_SHARE_PIN_SW);	// fall PERST_N pin (reset peripherals)
 #endif
+	mdelay(100);					// wait 100 ms pulse
 
 	val = 0;
 #if defined (CONFIG_PCIE_PORT0)

commit 8fd83c369764bd86f7672420321fcba2e6c111fb

ralink arch: fix MT7621 regression after [d05385b7dc12]

trunk/linux-3.0.x/arch/mips/rt2880/pci.c
@@ -615,8 +615,9 @@ int __init init_ralink_pci(void)
 	val |= (0x1<<GPIO_PCIE_PORT2);
 #endif
 	mdelay(50);
-	RALINK_GPIO_DATA0 &= ~(val);			// fall PERST_N pin (reset peripherals)
 	RALINK_GPIO_CTRL0 |= val;			// switch PERST_N pin to output mode
+	mdelay(50);
+	RALINK_GPIO_DATA0 &= ~(val);			// fall PERST_N pin (reset peripherals)
 #else /* !defined (GPIO_PERST) */
 	RALINK_GPIOMODE &= ~(0x3<<PCIE_SHARE_PIN_SW);	// fall PERST_N pin (reset peripherals)
 #endif

trunk/linux-3.4.x/arch/mips/rt2880/pci.c
@@ -615,8 +615,9 @@ int __init init_ralink_pci(void)
 	val |= (0x1<<GPIO_PCIE_PORT2);
 #endif
 	mdelay(50);
-	RALINK_GPIO_DATA0 &= ~(val);			// fall PERST_N pin (reset peripherals)
 	RALINK_GPIO_CTRL0 |= val;			// switch PERST_N pin to output mode
+	mdelay(50);
+	RALINK_GPIO_DATA0 &= ~(val);			// fall PERST_N pin (reset peripherals)
 #else /* !defined (GPIO_PERST) */
 	RALINK_GPIOMODE &= ~(0x3<<PCIE_SHARE_PIN_SW);	// fall PERST_N pin (reset peripherals)
 #endif