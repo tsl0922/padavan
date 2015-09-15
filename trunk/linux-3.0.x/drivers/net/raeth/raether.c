tsl0922/padavan

commit cc47680a12d15536522d88bb3d605e52ac8945e3

ralink arch: drop deprecated RTDEV_XXX params, use CONFIG_RT_FIRST_IF_XXX/CONFIG_RT_SECOND_IF_XXX to handle driver features

trunk/linux-3.0.x/ralink/Kconfig
@@ -345,19 +345,6 @@ config RT3XXX_EHCI_OHCI
 config MTK_XHCI
 	bool "MediaTek USB3/USB2/USB1.1 xHCI controller support"
 	depends on (RALINK_MT7621)
-
-config RTDEV_USB
-	bool
-	default y if (RT305x_INIC_USB || RT3572_AP || RT5572_AP)
-
-config RTDEV_MII
-	bool
-	default y if (RT305x_INIC_MII || RT3352_INIC_MII)
-
-config RTDEV_PCI
-	bool
-	default y if (RT3090_AP || RT5392_AP || RT5592_AP || RT3593_AP || MT7610_AP || MT76X2_AP || MT76X3_AP)
-	select WLAN
 endmenu
 
 endmenu

trunk/linux-3.4.x/ralink/Kconfig
@@ -346,19 +346,6 @@ config MTK_XHCI
 	bool "MediaTek USB3/USB2/USB1.1 xHCI controller support"
 	depends on (RALINK_MT7621)
 	select USB_ARCH_HAS_XHCI
-
-config RTDEV_USB
-	bool
-	default y if (RT305x_INIC_USB || RT3572_AP || RT5572_AP)
-
-config RTDEV_MII
-	bool
-	default y if (RT305x_INIC_MII || RT3352_INIC_MII)
-
-config RTDEV_PCI
-	bool
-	default y if (RT3090_AP || RT5392_AP || RT5592_AP || RT3593_AP || MT7610_AP || MT76X2_AP || MT76X3_AP)
-	select WLAN
 endmenu
 
 endmenu

trunk/proprietary/rt_ppe/hw_nat/ra_nat.c
@@ -357,7 +357,7 @@ uint32_t PpeExtIfRxHandler(struct sk_buff * skb)
 	switch (VirIfIdx) {
 	case DP_RA0:
 	case DP_RA1:
-#if defined (HWNAT_DP_RAI_AP)
+#if !defined (CONFIG_RT_SECOND_IF_NONE)
 	case DP_RAI0:
 	case DP_RAI1:
 #endif
@@ -875,41 +875,45 @@ uint32_t PpeSetExtIfNum(struct sk_buff *skb, struct FoeEntry* foe_entry)
 #define MIN_NET_DEVICE_FOR_APCLI	0x20
 #define MIN_NET_DEVICE_FOR_MESH		0x30
 
-#if defined (HWNAT_DP_RAI_AP)
+#if !defined (CONFIG_RT_SECOND_IF_NONE)
 	if (offset == DP_RAI0) {
-#if defined (HWNAT_DP_RAI_MESH)
+#if defined (HWNAT_USE_SECOND_IF_CBOFF)
+#if defined (HWNAT_USE_IF_MESH)
 		if (RTMP_GET_PACKET_IF(skb) >= MIN_NET_DEVICE_FOR_MESH)
 			offset = (RTMP_GET_PACKET_IF(skb) - MIN_NET_DEVICE_FOR_MESH + DP_MESHI0);
 		else
 #endif
-#if defined (HWNAT_DP_RAI_APCLI)
+#if defined (HWNAT_USE_IF_APCLI)
 		if (RTMP_GET_PACKET_IF(skb) >= MIN_NET_DEVICE_FOR_APCLI)
 			offset = (RTMP_GET_PACKET_IF(skb) - MIN_NET_DEVICE_FOR_APCLI + DP_APCLII0);
 		else
 #endif
-#if defined (HWNAT_DP_RAI_WDS)
+#if defined (HWNAT_USE_IF_WDS)
 		if (RTMP_GET_PACKET_IF(skb) >= MIN_NET_DEVICE_FOR_WDS)
 			offset = (RTMP_GET_PACKET_IF(skb) - MIN_NET_DEVICE_FOR_WDS + DP_WDSI0);
 		else
+#endif
 #endif
 			;
 	} else
 #endif
 	if (offset == DP_RA0) {
-#if defined (HWNAT_DP_RA_MESH)
+#if defined (HWNAT_USE_FIRST_IF_CBOFF)
+#if defined (HWNAT_USE_IF_MESH)
 		if (RTMP_GET_PACKET_IF(skb) >= MIN_NET_DEVICE_FOR_MESH)
 			offset = (RTMP_GET_PACKET_IF(skb) - MIN_NET_DEVICE_FOR_MESH + DP_MESH0);
 		else
 #endif
-#if defined (HWNAT_DP_RA_APCLI)
+#if defined (HWNAT_USE_IF_APCLI)
 		if (RTMP_GET_PACKET_IF(skb) >= MIN_NET_DEVICE_FOR_APCLI)
 			offset = (RTMP_GET_PACKET_IF(skb) - MIN_NET_DEVICE_FOR_APCLI + DP_APCLI0);
 		else
 #endif
-#if defined (HWNAT_DP_RA_WDS)
+#if defined (HWNAT_USE_IF_WDS)
 		if (RTMP_GET_PACKET_IF(skb) >= MIN_NET_DEVICE_FOR_WDS)
 			offset = (RTMP_GET_PACKET_IF(skb) - MIN_NET_DEVICE_FOR_WDS + DP_WDS0);
 		else
+#endif
 #endif
 			;
 	}
@@ -2376,8 +2380,9 @@ void PpeSetDstPort(uint32_t Ebl)
 	int i;
 
 	if (Ebl) {
+#if !defined (CONFIG_RT_FIRST_IF_NONE)
 		DstPort[DP_RA0] = ra_dev_get_by_name("ra0");
-#if defined (HWNAT_DP_RA_MBSS) || defined (CONFIG_MT76X2_AP_MBSS) || defined (CONFIG_MT76X3_AP_MBSS)
+#if defined (HWNAT_USE_IF_MBSS)
 		DstPort[DP_RA1] = ra_dev_get_by_name("ra1");
 		DstPort[DP_RA2] = ra_dev_get_by_name("ra2");
 		DstPort[DP_RA3] = ra_dev_get_by_name("ra3");
@@ -2386,21 +2391,23 @@ void PpeSetDstPort(uint32_t Ebl)
 		DstPort[DP_RA6] = ra_dev_get_by_name("ra6");
 		DstPort[DP_RA7] = ra_dev_get_by_name("ra7");
 #endif
-#if defined (HWNAT_DP_RA_WDS) || defined (CONFIG_MT76X2_AP_WDS) || defined (CONFIG_MT76X3_AP_WDS)
+#if defined (HWNAT_USE_IF_WDS)
 		DstPort[DP_WDS0] = ra_dev_get_by_name("wds0");
 		DstPort[DP_WDS1] = ra_dev_get_by_name("wds1");
 		DstPort[DP_WDS2] = ra_dev_get_by_name("wds2");
 		DstPort[DP_WDS3] = ra_dev_get_by_name("wds3");
 #endif
-#if defined (HWNAT_DP_RA_APCLI) || defined (CONFIG_MT76X2_AP_APCLI) || defined (CONFIG_MT76X3_AP_APCLI)
+#if defined (HWNAT_USE_IF_APCLI)
 		DstPort[DP_APCLI0] = ra_dev_get_by_name("apcli0");
 #endif
-#if defined (HWNAT_DP_RA_MESH) || defined (CONFIG_MT76X2_AP_MESH) || defined (CONFIG_MT76X3_AP_MESH)
+#if defined (HWNAT_USE_IF_MESH)
 		DstPort[DP_MESH0] = ra_dev_get_by_name("mesh0");
 #endif
-#if defined (HWNAT_DP_RAI_AP)
+#endif
+
+#if !defined (CONFIG_RT_SECOND_IF_NONE)
 		DstPort[DP_RAI0] = ra_dev_get_by_name("rai0");
-#if defined (HWNAT_DP_RAI_MBSS) || defined (CONFIG_MT76X2_AP_MBSS)
+#if defined (HWNAT_USE_IF_MBSS)
 		DstPort[DP_RAI1] = ra_dev_get_by_name("rai1");
 		DstPort[DP_RAI2] = ra_dev_get_by_name("rai2");
 		DstPort[DP_RAI3] = ra_dev_get_by_name("rai3");
@@ -2409,19 +2416,20 @@ void PpeSetDstPort(uint32_t Ebl)
 		DstPort[DP_RAI6] = ra_dev_get_by_name("rai6");
 		DstPort[DP_RAI7] = ra_dev_get_by_name("rai7");
 #endif
-#if defined (HWNAT_DP_RAI_WDS) || defined (CONFIG_MT76X2_AP_WDS)
+#if defined (HWNAT_USE_IF_WDS)
 		DstPort[DP_WDSI0] = ra_dev_get_by_name("wdsi0");
 		DstPort[DP_WDSI1] = ra_dev_get_by_name("wdsi1");
 		DstPort[DP_WDSI2] = ra_dev_get_by_name("wdsi2");
 		DstPort[DP_WDSI3] = ra_dev_get_by_name("wdsi3");
 #endif
-#if defined (HWNAT_DP_RAI_APCLI) || defined (CONFIG_MT76X2_AP_APCLI)
+#if defined (HWNAT_USE_IF_APCLI)
 		DstPort[DP_APCLII0] = ra_dev_get_by_name("apclii0");
 #endif
-#if defined (HWNAT_DP_RAI_MESH) || defined (CONFIG_MT76X2_AP_MESH)
+#if defined (HWNAT_USE_IF_MESH)
 		DstPort[DP_MESHI0] = ra_dev_get_by_name("meshi0");
 #endif
-#endif /* HWNAT_DP_RAI_AP */
+#endif
+
 		DstPort[DP_GMAC1] = ra_dev_get_by_name("eth2");
 #if defined (CONFIG_RAETH_GMAC2)
 		DstPort[DP_GMAC2] = ra_dev_get_by_name("eth3");

trunk/proprietary/rt_ppe/hw_nat/ra_nat.h
@@ -20,56 +20,6 @@
 
 #define HW_NAT_MODULE_VER "v2.51.9"
 
-/* ra0-15, wds0-4, apcli0, mesh0 interfaces */
-#if defined (CONFIG_RT2860V2_AP_MBSS)
-#define HWNAT_DP_RA_MBSS
-#endif
-
-#if defined (CONFIG_RT2860V2_AP_WDS)
-#define HWNAT_DP_RA_WDS
-#endif
-
-#if defined (CONFIG_RT2860V2_AP_APCLI)
-#define HWNAT_DP_RA_APCLI
-#endif
-
-#if defined (CONFIG_RT2860V2_AP_MESH)
-#define HWNAT_DP_RA_MESH
-#endif
-
-/* rai0-15, wdsi0-4, apclii0, meshi0 interfaces */
-#if defined (CONFIG_RTDEV_USB) || defined (CONFIG_RTDEV_PCI)
-#define HWNAT_DP_RAI_AP
-
-#if defined (CONFIG_RT3090_AP_MBSS) || defined (CONFIG_RT5392_AP_MBSS) || \
-    defined (CONFIG_RT3572_AP_MBSS) || defined (CONFIG_RT5572_AP_MBSS) || \
-    defined (CONFIG_RT5592_AP_MBSS) || defined (CONFIG_RT3593_AP_MBSS) || \
-    defined (CONFIG_MT7610_AP_MBSS)
-#define HWNAT_DP_RAI_MBSS
-#endif
-
-#if defined (CONFIG_RT3090_AP_WDS) || defined (CONFIG_RT5392_AP_WDS) || \
-    defined (CONFIG_RT3572_AP_WDS) || defined (CONFIG_RT5572_AP_WDS) || \
-    defined (CONFIG_RT5592_AP_WDS) || defined (CONFIG_RT3593_AP_WDS) || \
-    defined (CONFIG_MT7610_AP_WDS)
-#define HWNAT_DP_RAI_WDS
-#endif
-
-#if defined (CONFIG_RT3090_AP_APCLI) || defined (CONFIG_RT5392_AP_APCLI) || \
-    defined (CONFIG_RT3572_AP_APCLI) || defined (CONFIG_RT5572_AP_APCLI) || \
-    defined (CONFIG_RT5592_AP_APCLI) || defined (CONFIG_RT3593_AP_APCLI) || \
-    defined (CONFIG_MT7610_AP_APCLI)
-#define HWNAT_DP_RAI_APCLI
-#endif
-
-#if defined (CONFIG_RT3090_AP_MESH) || defined (CONFIG_RT5392_AP_MESH) || \
-    defined (CONFIG_RT3572_AP_MESH) || defined (CONFIG_RT5572_AP_MESH) || \
-    defined (CONFIG_RT5592_AP_MESH) || defined (CONFIG_RT3593_AP_MESH) || \
-    defined (CONFIG_MT7610_AP_MESH)
-#define HWNAT_DP_RAI_MESH
-#endif
-#endif
-
 /*
  * TYPEDEFS AND STRUCTURES
  */
@@ -96,7 +46,7 @@ enum DstPort {
 	DP_WDS3 = 30,
 	DP_APCLI0 = 31,
 	DP_MESH0 = 32,
-#if defined (HWNAT_DP_RAI_AP)
+#if !defined (CONFIG_RT_SECOND_IF_NONE)
 	DP_RAI0 = 33,
 	DP_RAI1 = 34,
 	DP_RAI2 = 35,

trunk/proprietary/rt_ppe/hw_nat/ra_nat_compat.h
@@ -41,4 +41,50 @@
 
 ///////////////////////////////////////////////////////////////
 
+#if defined (CONFIG_RT3090_AP_MBSS) || defined (CONFIG_RT5392_AP_MBSS) || \
+    defined (CONFIG_RT5592_AP_MBSS) || defined (CONFIG_RT3593_AP_MBSS) || \
+    defined (CONFIG_MT7610_AP_MBSS) || defined (CONFIG_MT76X2_AP_MBSS) || \
+    defined (CONFIG_MT76X3_AP_MBSS) || defined (CONFIG_MT7615_AP_MBSS) || \
+    defined (CONFIG_RT2860V2_AP_MBSS)
+#define HWNAT_USE_IF_MBSS
+#endif
+
+#if defined (CONFIG_RT3090_AP_WDS) || defined (CONFIG_RT5392_AP_WDS) || \
+    defined (CONFIG_RT5592_AP_WDS) || defined (CONFIG_RT3593_AP_WDS) || \
+    defined (CONFIG_MT7610_AP_WDS) || defined (CONFIG_MT76X2_AP_WDS) || \
+    defined (CONFIG_MT76X3_AP_WDS) || defined (CONFIG_MT7615_AP_WDS) || \
+    defined (CONFIG_RT2860V2_AP_WDS)
+#define HWNAT_USE_IF_WDS
+#endif
+
+#if defined (CONFIG_RT3090_AP_APCLI) || defined (CONFIG_RT5392_AP_APCLI) || \
+    defined (CONFIG_RT5592_AP_APCLI) || defined (CONFIG_RT3593_AP_APCLI) || \
+    defined (CONFIG_MT7610_AP_APCLI) || defined (CONFIG_MT76X2_AP_APCLI) || \
+    defined (CONFIG_MT76X3_AP_APCLI) || defined (CONFIG_MT7615_AP_APCLI) || \
+    defined (CONFIG_RT2860V2_AP_APCLI)
+#define HWNAT_USE_IF_APCLI
+#endif
+
+#if defined (CONFIG_RT3090_AP_MESH) || defined (CONFIG_RT5392_AP_MESH) || \
+    defined (CONFIG_RT5592_AP_MESH) || defined (CONFIG_RT3593_AP_MESH) || \
+    defined (CONFIG_MT7610_AP_MESH) || defined (CONFIG_MT76X2_AP_MESH) || \
+    defined (CONFIG_MT76X3_AP_MESH) || defined (CONFIG_MT7615_AP_MESH) || \
+    defined (CONFIG_RT2860V2_AP_MESH)
+#define HWNAT_USE_IF_MESH
+#endif
+
+/* old drivers send skb via ra0/rai0 and use skb->cb[CB_OFF+6] for store type */
+#if defined (CONFIG_RT_FIRST_IF_RT3090) || defined (CONFIG_RT_FIRST_IF_RT5392) || \
+    defined (CONFIG_RT_FIRST_IF_RT5592) || defined (CONFIG_RT_FIRST_IF_RT3593) || \
+    defined (CONFIG_RT_FIRST_IF_MT7610E) || defined (CONFIG_RT_FIRST_IF_RT2860)
+#define HWNAT_USE_FIRST_IF_CBOFF
+#endif
+
+#if defined (CONFIG_RT_SECOND_IF_RT3090) || defined (CONFIG_RT_SECOND_IF_RT5392) || \
+    defined (CONFIG_RT_SECOND_IF_RT5592) || defined (CONFIG_RT_SECOND_IF_RT3593) || \
+    defined (CONFIG_RT_SECOND_IF_MT7610E)
+#define HWNAT_USE_SECOND_IF_CBOFF
+#endif
+
+
 #endif

commit e17a059d4e532beae00882e1f0bacd39dbde8078

ralink arch: fix N65U iNIC_mii regression after [cc47680a12]

trunk/linux-3.0.x/drivers/net/raeth/raether.c
@@ -134,7 +134,7 @@ fill_hw_vlan_tx_map(END_DEVICE *ei_local)
 	ei_local->vlan_id_map[i] = (u16)DP_RA1;		// IDX: 12
 	ei_local->vlan_4k_map[DP_RA1] = (u8)i;
 	i++;
-#if !defined (CONFIG_RT_SECOND_IF_NONE)
+#if defined (HWNAT_USE_SECOND_IF)
 	ei_local->vlan_id_map[i] = (u16)DP_RAI0;	// IDX: 13
 	ei_local->vlan_4k_map[DP_RAI0] = (u8)i;
 	i++;

trunk/linux-3.0.x/ralink/Kconfig
@@ -376,6 +376,11 @@ config MTK_XHCI
 	bool "MediaTek USB3/USB2/USB1.1 xHCI controller support"
 	depends on (RALINK_MT7621)
 #	select USB_ARCH_HAS_XHCI
+
+# used only for iNIC_mii binary blob (need align Linux kernel structures to original environment)
+config RTDEV_MII
+	bool
+	default y if RT3352_INIC_MII
 endmenu
 
 endmenu

trunk/linux-3.4.x/drivers/net/raeth/raether.c
@@ -134,7 +134,7 @@ fill_hw_vlan_tx_map(END_DEVICE *ei_local)
 	ei_local->vlan_id_map[i] = (u16)DP_RA1;		// IDX: 12
 	ei_local->vlan_4k_map[DP_RA1] = (u8)i;
 	i++;
-#if !defined (CONFIG_RT_SECOND_IF_NONE)
+#if defined (HWNAT_USE_SECOND_IF)
 	ei_local->vlan_id_map[i] = (u16)DP_RAI0;	// IDX: 13
 	ei_local->vlan_4k_map[DP_RAI0] = (u8)i;
 	i++;

trunk/linux-3.4.x/ralink/Kconfig
@@ -376,6 +376,11 @@ config MTK_XHCI
 	bool "MediaTek USB3/USB2/USB1.1 xHCI controller support"
 	depends on (RALINK_MT7621)
 	select USB_ARCH_HAS_XHCI
+
+# used only for iNIC_mii binary blob (need align Linux kernel structures to original environment)
+config RTDEV_MII
+	bool
+	default y if RT3352_INIC_MII
 endmenu
 
 endmenu

trunk/proprietary/rt_ppe/hw_nat/ra_nat.c
@@ -357,7 +357,7 @@ uint32_t PpeExtIfRxHandler(struct sk_buff * skb)
 	switch (VirIfIdx) {
 	case DP_RA0:
 	case DP_RA1:
-#if !defined (CONFIG_RT_SECOND_IF_NONE)
+#if defined (HWNAT_USE_SECOND_IF)
 	case DP_RAI0:
 	case DP_RAI1:
 #endif
@@ -875,7 +875,7 @@ uint32_t PpeSetExtIfNum(struct sk_buff *skb, struct FoeEntry* foe_entry)
 #define MIN_NET_DEVICE_FOR_APCLI	0x20
 #define MIN_NET_DEVICE_FOR_MESH		0x30
 
-#if !defined (CONFIG_RT_SECOND_IF_NONE)
+#if defined (HWNAT_USE_SECOND_IF)
 	if (offset == DP_RAI0) {
 #if defined (HWNAT_USE_SECOND_IF_CBOFF)
 #if defined (HWNAT_USE_IF_MESH)
@@ -2380,7 +2380,6 @@ void PpeSetDstPort(uint32_t Ebl)
 	int i;
 
 	if (Ebl) {
-#if !defined (CONFIG_RT_FIRST_IF_NONE)
 		DstPort[DP_RA0] = ra_dev_get_by_name("ra0");
 #if defined (HWNAT_USE_IF_MBSS)
 		DstPort[DP_RA1] = ra_dev_get_by_name("ra1");
@@ -2403,9 +2402,8 @@ void PpeSetDstPort(uint32_t Ebl)
 #if defined (HWNAT_USE_IF_MESH)
 		DstPort[DP_MESH0] = ra_dev_get_by_name("mesh0");
 #endif
-#endif
 
-#if !defined (CONFIG_RT_SECOND_IF_NONE)
+#if defined (HWNAT_USE_SECOND_IF)
 		DstPort[DP_RAI0] = ra_dev_get_by_name("rai0");
 #if defined (HWNAT_USE_IF_MBSS)
 		DstPort[DP_RAI1] = ra_dev_get_by_name("rai1");

trunk/proprietary/rt_ppe/hw_nat/ra_nat.h
@@ -20,6 +20,10 @@
 
 #define HW_NAT_MODULE_VER "v2.51.9"
 
+#if !defined (CONFIG_RT_SECOND_IF_NONE) && !defined (CONFIG_RT_SECOND_IF_RT3352_INIC)
+#define HWNAT_USE_SECOND_IF
+#endif
+
 /*
  * TYPEDEFS AND STRUCTURES
  */
@@ -46,7 +50,7 @@ enum DstPort {
 	DP_WDS3 = 30,
 	DP_APCLI0 = 31,
 	DP_MESH0 = 32,
-#if !defined (CONFIG_RT_SECOND_IF_NONE)
+#if defined (HWNAT_USE_SECOND_IF)
 	DP_RAI0 = 33,
 	DP_RAI1 = 34,
 	DP_RAI2 = 35,