6d29f0f4618226204fdd36500d76c6e7439be9f5
Shuanglei Tao <tsl0922@gmail.com>
2022-09-30 20:42:43 +0800

    cmake: use shared toolchain file

--- trunk/cmake/cross-mipsel-linux.cmake
+++ trunk/cmake/cross-mipsel-linux.cmake
@@ -3,20 +3,19 @@
 #
 # This can be used when running cmake in the following way:
 #  cd build/
-#  cmake .. -DCMAKE_TOOLCHAIN_FILE=../cross-mipsel-linux.cmake
+#  cmake .. -DCMAKE_TOOLCHAIN_FILE=$(ROOTDIR)/cmake/cross-mipsel-linux.cmake
 
-set(CROSS_PATH $ENV{CONFIG_CROSS_COMPILER_ROOT})
 set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:$ENV{STAGEDIR}/lib/pkgconfig")
 
 # Target operating system name.
 set(CMAKE_SYSTEM_NAME Linux)
 
 # Name of C compiler.
-set(CMAKE_C_COMPILER "${CROSS_PATH}/bin/mipsel-linux-uclibc-gcc")
-set(CMAKE_CXX_COMPILER "${CROSS_PATH}/bin/mipsel-linux-uclibc-g++")
+set(CMAKE_C_COMPILER "$ENV{CROSS_COMPILE}gcc")
+set(CMAKE_CXX_COMPILER "$ENV{CROSS_COMPILE}g++")
 
 # Where to look for the target environment. (More paths can be added here)
-set(CMAKE_FIND_ROOT_PATH "${CROSS_PATH}" "$ENV{STAGEDIR}")
+set(CMAKE_FIND_ROOT_PATH "$ENV{CONFIG_CROSS_COMPILER_ROOT}" "$ENV{STAGEDIR}")
 
 # Adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment only.
