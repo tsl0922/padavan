856a6f083777fa616d216aa97fe29f7657829265
TurBoTse <6028824+TurBoTse@users.noreply.github.com>
2022-10-18 13:13:37 +0800

    boards: add RE-CP-02

---
+++ trunk/configs/boards/RE-CP-02/board.h
@@ -0,0 +1,20 @@
+#define BOARD_PID "RE-CP-02"
+#define BOARD_NAME "RE-CP-02"
+#define BOARD_DESC "AX1800 Wireless Router"
+#define BOARD_VENDOR_NAME "Ruiji Technology Co., Ltd."
+#define BOARD_VENDOR_URL "https://www.ruiji.com/"
+#define BOARD_MODEL_URL "https://www.ruiji.com/"
+#define BOARD_BOOT_TIME 25
+#define BOARD_FLASH_TIME 120
+
+#define BOARD_HAS_5G_11AC 1
+#define BOARD_HAS_5G_11AX 1
+#define BOARD_HAS_2G_11AX 1
+#define BOARD_NUM_ANT_5G_TX 2
+#define BOARD_NUM_ANT_5G_RX 2
+#define BOARD_NUM_ANT_2G_TX 2
+#define BOARD_NUM_ANT_2G_RX 2
+#define BOARD_NUM_ETH_LEDS 0
+#define BOARD_NUM_ETH_EPHY 4
+#define BOARD_HAS_EPHY_L1000 1
+#define BOARD_HAS_EPHY_W1000 1
