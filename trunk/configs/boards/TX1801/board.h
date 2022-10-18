0e66727ad28ed957cb979edcd10d06709b3c34fa
TurBoTse <6028824+TurBoTse@users.noreply.github.com>
2022-10-18 13:11:59 +0800

    boards: add TX1801 Plus

---
+++ trunk/configs/boards/TX1801/board.h
@@ -0,0 +1,22 @@
+/* H3C TX1801 Plus */
+
+#define BOARD_PID		"H3C TX1801 Plus"
+#define BOARD_NAME		"H3C TX1801 Plus"
+#define BOARD_DESC		"H3C TX1801 Plus Wireless Router"
+#define BOARD_VENDOR_NAME	"H3C"
+#define BOARD_VENDOR_URL	"http://www.h3c.com.cn/cn/"
+#define BOARD_MODEL_URL		"http://www.h3c.com.cn/cn/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC		1
+#define BOARD_HAS_5G_11AX		1
+#define BOARD_HAS_2G_11AX		1
+#define BOARD_NUM_ANT_5G_TX		2
+#define BOARD_NUM_ANT_5G_RX		2
+#define BOARD_NUM_ANT_2G_TX		2
+#define BOARD_NUM_ANT_2G_RX		2
+#define BOARD_NUM_ETH_LEDS		0
+#define BOARD_NUM_ETH_EPHY		4
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
