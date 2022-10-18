241e545e351e66bcbc7c9648b908237edf3af08a
TurBoTse <6028824+TurBoTse@users.noreply.github.com>
2022-10-18 13:01:23 +0800

    boards: add MI-R4A

---
+++ trunk/configs/boards/MI-R4A/board.h
@@ -0,0 +1,22 @@
+/* Xiaomi MI-R4A */
+
+#define BOARD_PID		"MI-R4A"
+#define BOARD_NAME		"MI-R4A"
+#define BOARD_DESC		"Xiaomi Mi R4A Wireless Router"
+#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"https://www.mi.com/"
+#define BOARD_MODEL_URL		"https://www.mi.com/miwifi4ac"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP	0
