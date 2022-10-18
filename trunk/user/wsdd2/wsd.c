4a632c47c1f61608a99f275f39a78f3ee1851971
TurBoTse <6028824+TurBoTse@users.noreply.github.com>
2022-10-19 02:03:20 +0800

    wsdd2: padavan-ng

--- trunk/user/wsdd2/wsd.c
+++ trunk/user/wsdd2/wsd.c
@@ -151,6 +151,14 @@ int set_getresp(const char *str, const char **next)
 	const char *p, *val;
 	size_t keylen, vallen;
 
+	if (str == NULL) {
+	    return -1;
+	}
+
+	if (*str == '\0') {
+	    return -1;
+	}
+
 	/* Trim leading space. */
 	while (*str && isspace(*str))
 		str++;
@@ -972,7 +980,8 @@ int wsd_init(struct endpoint *ep)
 	workgroup = nvram_safe_get("st_samba_workgroup");
 	if (strlen(workgroup) == 0) workgroup = "WORKGROUP";
 
-	netbiosname = hostname;
+	if (!netbiosname)
+		netbiosname = hostname;
 
 	if (!getresp_inited)
 		init_getresp();
