025c78eb01bb346cf131262dee382756da22bdd5
TurBoTse <6028824+TurBoTse@users.noreply.github.com>
2022-10-18 12:51:47 +0800

    boards: add EA7500

---
+++ trunk/configs/boards/EA7500/board.h
@@ -0,0 +1,21 @@
+/* EA7500 */
+
+#define BOARD_PID		"EA7500"
+#define BOARD_NAME		"EA7500"
+#define BOARD_DESC		"EA7500 Wireless Router"
+#define BOARD_VENDOR_NAME	"Linksys Co., Ltd."
+#define BOARD_VENDOR_URL	"https://www.linksys.com/cn/"
+#define BOARD_MODEL_URL        "https://www.linksys.com/cn/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_HAS_5G_11AC		1
+#define BOARD_NUM_ANT_5G_TX		4
+#define BOARD_NUM_ANT_5G_RX		4
+#define BOARD_NUM_ANT_2G_TX		4
+#define BOARD_NUM_ANT_2G_RX		4
+#define BOARD_NUM_ETH_LEDS		1
+#define BOARD_NUM_ETH_EPHY		5
+#define BOARD_HAS_EPHY_L1000		1
+#define BOARD_HAS_EPHY_W1000		1
+#define BOARD_NUM_UPHY_USB3		1
+#define BOARD_USB_PORT_SWAP		1
