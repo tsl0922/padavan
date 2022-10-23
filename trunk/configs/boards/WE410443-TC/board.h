928b94e2683def7ad44b466a7f878b8ae0ef15bc
TurBoTse <6028824+TurBoTse@users.noreply.github.com>
2022-10-23 14:05:39 +0800

    boards: add WE410443-TC
    Co-Authored-By: akw28888 <8659165+akw28888@users.noreply.github.com>

---
+++ trunk/configs/boards/WE410443-TC/board.h
@@ -0,0 +1,21 @@
+/* ZTE-E8820S */
+
+#define BOARD_PID		"WE410443-TC"
+#define BOARD_NAME		"WE410443-TC"
+#define BOARD_DESC		"Arcadyan WE410443-TC Wireless Router"
+#define BOARD_VENDOR_NAME	"Arcadyan Technology Corporation"
+#define BOARD_VENDOR_URL	"https://www.arcadyan.com/"
+#define BOARD_MODEL_URL		"https://www.arcadyan.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME   	120
+
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	4
+#define BOARD_NUM_ANT_5G_RX	4
+#define BOARD_NUM_ANT_2G_TX	4
+#define BOARD_NUM_ANT_2G_RX	4
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP     0
