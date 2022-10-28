355cd715486fecda0eae99ba3b5b2664ddaa0460
TurBoTse <6028824+TurBoTse@users.noreply.github.com>
2022-10-28 20:38:13 +0800

    Board: add ZTE-E8820V2
    Co-Authored-By: monw <65690955+monw@users.noreply.github.com>

---
+++ trunk/configs/boards/E8820V2/board.h
@@ -0,0 +1,20 @@
+/* ZTE E8820V2 */
+
+#define BOARD_PID		"E8820V2"
+#define BOARD_NAME		"E8820V2"
+#define BOARD_DESC		"ZTE E8820V2 Wireless Router"
+#define BOARD_VENDOR_NAME	"ZTE Corporation"
+#define BOARD_VENDOR_URL	"http://www.ztehome.com.cn/"
+#define BOARD_MODEL_URL		"http://www.ztehome.com.cn/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+
