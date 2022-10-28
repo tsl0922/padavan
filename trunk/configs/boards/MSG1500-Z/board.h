57f5768419978537fbcda1674a2aacc8e9d6afca
TurBoTse <6028824+TurBoTse@users.noreply.github.com>
2022-10-28 20:37:15 +0800

    Board: add MSG1500-Z.00
    Co-Authored-By: monw <65690955+monw@users.noreply.github.com>

---
+++ trunk/configs/boards/MSG1500-Z/board.h
@@ -0,0 +1,20 @@
+/* MSG1500 */
+
+#define BOARD_PID		"MSG1500-Z"
+#define BOARD_NAME		"MSG1500-Z"
+#define BOARD_DESC		"RAISECOM MSG1500-Z Wireless Router"
+#define BOARD_VENDOR_NAME	"RAISECOM"
+#define BOARD_VENDOR_URL	"https://www.raisecom.com/"
+#define BOARD_MODEL_URL		"https://www.raisecom.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+
