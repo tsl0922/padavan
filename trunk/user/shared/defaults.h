771a794e21f822027e8bb9928126276fa173a5f5
Shuanglei Tao <tsl0922@gmail.com>
2023-06-28 09:37:37 +0800

    update default 5G country code

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -36,7 +36,7 @@
 #define DEF_LAN_MASK		"255.255.255.0"
 
 #define DEF_WLAN_2G_CC		"CN"
-#define DEF_WLAN_5G_CC		"US"
+#define DEF_WLAN_5G_CC		"CN"
 #define DEF_WLAN_2G_SSID	BOARD_PID "_%s"
 #define DEF_WLAN_5G_SSID	BOARD_PID "_5G_%s"
 #define DEF_WLAN_2G_GSSID	BOARD_PID "_GUEST_%s"
